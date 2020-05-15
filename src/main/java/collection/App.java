90eee36e28d79d009e92c80bba22e81abf4d3cf7
VladimirGlinskikh <support@zhelezyaka.kz>
2020-05-15 23:36:52 +0600

    key uniqueness is required in HashMap, value uniqueness in HashMap is optional

--- src/main/java/collection/App.java
+++ src/main/java/collection/App.java
@@ -6,7 +6,11 @@ import java.util.Map;
 public class App {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
-        map.put("key", 17);
+
+        System.out.println( map.put("килька", 17));
+        System.out.println( map.put("сардина", 14));
+        System.out.println( map.put("сардина", 7));
+
         System.out.println(map);
         System.out.println(map.size());
         System.out.println(map.isEmpty());
