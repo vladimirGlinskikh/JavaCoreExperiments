5a3123d636a357670dc610a36ee7f31e34f92a20
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-10 21:18:38 +0600

    It is the test program showing how to instantiate and use the inner class

---
+++ src/main/java/inner/anonymousInner/nested/InnerClassTest.java
@@ -0,0 +1,33 @@
+package inner.anonymousInner.nested;
+
+import inner.anonymousInner.nested.OuterClass.*;
+
+import java.util.Arrays;
+
+public class InnerClassTest {
+    public static void main(String[] args) {
+        OuterClass outer = new OuterClass(1, 2, 3, 4);
+
+        //static nested classes example
+        StaticNestedClass staticNestedClass = new StaticNestedClass();
+        StaticNestedClass staticNestedClass1 = new StaticNestedClass();
+
+        System.out.println(staticNestedClass.getName());
+        staticNestedClass.d = 11;
+        System.out.println(staticNestedClass.d);
+        System.out.println(staticNestedClass1.d);
+
+        //inner class example
+        InnerClass innerClass = outer.new InnerClass();
+        System.out.println(innerClass.getName());
+        System.out.println(innerClass);
+        innerClass.setValues();
+        System.out.println(innerClass);
+
+        //calling method using local inner class
+        outer.print("Outer");
+
+        //calling method using anonymous inner class
+        System.out.println(Arrays.toString(outer.getFilesInDir("./src/main/java/inner/anonymousInner/nested", ".java")));
+    }
+}
