ab3365f6d4609fd970e8e693bfe03e9599a7ce41
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-11 22:34:29 +0600

    Builder design pattern in action

---
+++ src/main/java/designPatterns/builder/TestBuilderPattern.java
@@ -0,0 +1,12 @@
+package designPatterns.builder;
+
+public class TestBuilderPattern {
+    public static void main(String[] args) {
+        Computer computer = new Computer.ComputerBuilder(
+                "500Gb", "4Gb")
+                .setWiFiEnabled(true)
+                .setGraphicsCardEnable(true)
+                .build();
+        System.out.println(computer);
+    }
+}
