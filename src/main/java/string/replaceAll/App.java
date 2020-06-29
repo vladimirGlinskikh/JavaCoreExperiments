bc26e77734018c5ffab4731d9a39a9470a773064
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-29 21:38:59 +0600

    Hiding numeric digits in a text

--- src/main/java/string/replaceAll/App.java
+++ src/main/java/string/replaceAll/App.java
@@ -2,10 +2,10 @@ package string.replaceAll;
 
 public class App {
     public static void main(String[] args) {
-        String string = "Welcome to JavaExperiments";
+        String string = "Hello Java! This is the number: 12345";
         System.out.println("Original String: " + string);
         System.out.println("--------");
-        String replace = string.replaceAll(" ", "");
+        String replace = string.replaceAll("[0-9]", "*");
         System.out.println("New String: " + replace);
     }
 }
