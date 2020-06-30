6e47804366659bee2bde582bdadea5b0cdf947f9
VladimirGlinskikh <support@zhelezyaka.kz>
2020-06-30 18:26:31 +0600

    Object creation

---
+++ src/main/java/objects/creation/Point.java
@@ -0,0 +1,20 @@
+package objects.creation;
+
+public class Point {
+    int x, y;
+
+    public Point() {
+        System.out.println("default constructor");
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static Point origin = new Point(0, 0);
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
