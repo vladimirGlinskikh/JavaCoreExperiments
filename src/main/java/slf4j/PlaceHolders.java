496e3f4320b6a04defa4c7a672446ae643ec4893
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-05 23:28:38 +0600

    Two argument variant

--- src/main/java/slf4j/PlaceHolders.java
+++ src/main/java/slf4j/PlaceHolders.java
@@ -3,10 +3,22 @@ package slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Scanner;
+
 public class PlaceHolders {
     public static void main(String[] args) {
+        Integer oldWeight;
+        Integer newWeight;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter old weight:");
+        oldWeight = scanner.nextInt();
+        System.out.println("Enter new weight:");
+        newWeight = scanner.nextInt();
+
         Logger logger = LoggerFactory.getLogger(PlaceHolders.class);
-        Integer age = 35;
-        logger.info("At the age of {} Vladimir got his first jog", age );
+
+        logger.info("Old weight is {}. new weight is {}.", oldWeight, newWeight);
+        logger.info("After the program weight reduced is: " + (oldWeight - newWeight));
     }
 }
