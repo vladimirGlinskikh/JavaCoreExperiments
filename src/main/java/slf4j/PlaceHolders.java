430230b130c471bd932bf5c54b6ab4779b667dd4
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-05 23:35:29 +0600

    Multiple argument variant

--- src/main/java/slf4j/PlaceHolders.java
+++ src/main/java/slf4j/PlaceHolders.java
@@ -7,18 +7,25 @@ import java.util.Scanner;
 
 public class PlaceHolders {
     public static void main(String[] args) {
-        Integer oldWeight;
-        Integer newWeight;
+//        Integer oldWeight;
+//        Integer newWeight;
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter old weight:");
+//        oldWeight = scanner.nextInt();
+//        System.out.println("Enter new weight:");
+//        newWeight = scanner.nextInt();
+//
+//        Logger logger = LoggerFactory.getLogger(PlaceHolders.class);
+//
+//        logger.info("Old weight is {}. new weight is {}.", oldWeight, newWeight);
+//        logger.info("After the program weight reduced is: " + (oldWeight - newWeight));
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter old weight:");
-        oldWeight = scanner.nextInt();
-        System.out.println("Enter new weight:");
-        newWeight = scanner.nextInt();
+        String month = "August";
+        String destination = "Software developer";
+        String company = "T2Studio";
 
         Logger logger = LoggerFactory.getLogger(PlaceHolders.class);
-
-        logger.info("Old weight is {}. new weight is {}.", oldWeight, newWeight);
-        logger.info("After the program weight reduced is: " + (oldWeight - newWeight));
+        logger.info("At the month of {} Vladimir got his first job as a {} at {}", month, destination, company);
     }
 }
