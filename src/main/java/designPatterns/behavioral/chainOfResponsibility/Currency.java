731256a7273077140f3ec36365fb43e765910250
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-07-08 14:45:55 +0600

    Chain of Responsibility pattern example

---
+++ src/main/java/designPatterns/behavioral/chainOfResponsibility/Currency.java
@@ -0,0 +1,13 @@
+package designPatterns.behavioral.chainOfResponsibility;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
