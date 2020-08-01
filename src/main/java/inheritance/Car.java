a1004474bd8cebf2ade4b23a00caa6513a34b128
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-01 15:20:58 +0600

    The Car class is derived from the Vehicle class

---
+++ src/main/java/inheritance/Car.java
@@ -0,0 +1,7 @@
+package inheritance;
+
+public class Car extends Vehicle {
+    public Car(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
