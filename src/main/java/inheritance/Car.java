aec6cf518c3fad40d48a8c4fa47981e6595498bd
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-01 16:32:36 +0600

    Subtyping polymorphism in action

--- src/main/java/inheritance/Car.java
+++ src/main/java/inheritance/Car.java
@@ -1,6 +1,7 @@
 package inheritance;
 
 public class Car extends Vehicle {
+
     public Car(String name) {
         super(name);
     }
--- src/main/java/inheritance/Vehicle.java
+++ src/main/java/inheritance/Vehicle.java
@@ -6,4 +6,9 @@ public class Vehicle {
     public Vehicle(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString(){
+        return "Vehicle: " + name;
+    }
 }
