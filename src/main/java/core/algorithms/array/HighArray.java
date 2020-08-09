d46edf218d2fa6bb6d2a4c249f8b6205ccbf67bb
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-09 21:39:59 +0600

    The example of dividing the program into classes has been improved

---
+++ src/main/java/core/algorithms/array/HighArray.java
@@ -0,0 +1,48 @@
+package core.algorithms.array;
+
+public class HighArray {
+    private long[] arr;
+    private int nElems;
+
+    public HighArray(int max) {
+        arr = new long[max];
+        nElems = 0;
+    }
+
+    public boolean find(long searchKey) {
+        int j;
+        for (j = 0; j < nElems; j++)
+            if (arr[j] == searchKey)
+                break;
+        if (j == nElems)
+            return false;
+        else
+            return true;
+    }
+
+    public void insert(long value) {
+        arr[nElems] = value;
+        nElems++;
+    }
+
+    public boolean delete(long value) {
+        int j;
+        for (j = 0; j < nElems; j++)
+            if (value == arr[j])
+                break;
+        if (j == nElems)
+            return false;
+        else {
+            for (int k = j; k < nElems; k++)
+                arr[k] = arr[k + 1];
+            nElems--;
+            return true;
+        }
+    }
+
+    public void display() {
+        for (int j = 0; j < nElems; j++)
+            System.out.print(arr[j] + " ");
+        System.out.println("");
+    }
+}
