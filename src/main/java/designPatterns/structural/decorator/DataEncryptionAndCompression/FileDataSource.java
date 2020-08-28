fd3b36de7518d95591c6a6f27d212fe362250a90
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-28 23:51:09 +0600

    Decorator pattern in action

---
+++ src/main/java/designPatterns/structural/decorator/DataEncryptionAndCompression/FileDataSource.java
@@ -0,0 +1,34 @@
+package designPatterns.structural.decorator.DataEncryptionAndCompression;
+
+import java.io.*;
+
+public class FileDataSource implements DataSource {
+    private String name;
+
+    public FileDataSource(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void writeData(String data) {
+        File file = new File(name);
+        try (OutputStream fos = new FileOutputStream(file)) {
+            fos.write(data.getBytes(), 0, data.length());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public String readData() {
+        char[] buffer = null;
+        File file = new File(name);
+        try (FileReader reader = new FileReader(file)) {
+            buffer = new char[(int) file.length()];
+            reader.read(buffer);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return new String(buffer);
+    }
+}
