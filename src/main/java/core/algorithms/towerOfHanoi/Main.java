8cc44899ba8107e3e3f6e602ecdbf37718d8b2fc
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-30 23:10:49 +0600

    Tower of Hanoi implementation

---
+++ src/main/java/core/algorithms/towerOfHanoi/Main.java
@@ -0,0 +1,18 @@
+package core.algorithms.towerOfHanoi;
+
+public class Main {
+    static void towerOfHanoi(int n, char fromRod, char toRod, char helperRod) {
+        if (n == 1) {
+            System.out.println("Take disk 1 from rod " + fromRod + " to rod " + toRod);
+            return;
+        }
+        towerOfHanoi(n - 1, fromRod, helperRod, toRod);
+        System.out.println("Take disk " + n + " from rod " + fromRod + " to rod " + toRod);
+        towerOfHanoi(n - 1, helperRod, toRod, fromRod);
+    }
+
+    public static void main(String args[]) {
+        int n = 5;
+        towerOfHanoi(n, 'A', 'C', 'B');
+    }
+}
