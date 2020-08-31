0b1d9eeafdcacb0b67dcf00a0df299b12c49303a
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-08-31 23:03:02 +0600

    Command pattern in action

---
+++ src/main/java/designPatterns/behavioral/command/commands/CopyCommand.java
@@ -0,0 +1,13 @@
+package designPatterns.behavioral.command.commands;
+
+public class CopyCommand extends Command {
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
