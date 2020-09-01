3263336fe69edf4458f88ed09f3adc18ae0bdc3b
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-02 00:39:35 +0600

    Shadowing of a Type Declaration by Another Type Declaration

---
+++ src/main/java/core/shadowingAndObscuring/shadowing/Test1.java
@@ -0,0 +1,12 @@
+package core.shadowingAndObscuring.shadowing;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Vector vector = new Vector();
+        System.out.println(vector.val[0]);
+    }
+}
+
+class Vector {
+    int val[] = {1, 2, 3};
+}
