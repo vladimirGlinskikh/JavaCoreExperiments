e0c45e493e0d6e9f055d305413f96c486f475b6a
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-04 20:34:53 +0600

    Task check done

--- src/main/java/stepic/mapBasicMethods/MapTest.java
+++ src/main/java/stepic/mapBasicMethods/MapTest.java
@@ -1,6 +1,8 @@
 package stepic.mapBasicMethods;
 
 import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class MapTest {
 
@@ -10,5 +12,23 @@ public class MapTest {
         }
         map.remove("c");
         return map;
+    }
+
+    public static SortedMap<String, Integer> wordCount(String[] strings) {
+        SortedMap<String, Integer> map = new TreeMap<>();
+        for (String s : strings) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        return map;
+    }
+
+    public static void printMap(Map<String, Integer> map) {
+        for (Map.Entry<String, Integer> e : map.entrySet()) {
+            System.out.format("%s : %s%n", e.getKey(), e.getValue());
+        }
     }
 }
