f9ea647c5f78b51d4d709bff1816fc449f69aa86
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-04 20:29:36 +0600

    Task check done

---
+++ src/main/java/stepic/mapBasicMethods/MapTest.java
@@ -0,0 +1,14 @@
+package stepic.mapBasicMethods;
+
+import java.util.Map;
+
+public class MapTest {
+
+    public static Map<String, String> mapShare(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+        }
+        map.remove("c");
+        return map;
+    }
+}
---
+++ src/main/java/stepic/mapBasicMethods/task.md
@@ -0,0 +1,4 @@
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that same value. 
+In all cases remove the key "c",leaving the rest of the map unchanged.
+
+{"a":"abstraction","b":"boolean","c":"xyz"} -> {"a":"abstraction","b":"abstraction"}
