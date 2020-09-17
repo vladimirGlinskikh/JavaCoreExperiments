451eeddb2e5435f7aa1648add8944fca847ca575
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-17 21:44:42 +0600

    Determining the size of an array

---
+++ src/main/java/core/algorithms/array/ArrayLength.java
@@ -0,0 +1,9 @@
+package core.algorithms.array;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        int[] intArray = {0, 2, 4, 6, 1};
+        int arrayLength = intArray.length;
+        System.out.println(arrayLength);
+    }
+}
