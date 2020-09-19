506afd83c0537021ad55fb38555371b9c9576d69
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-19 21:52:09 +0600

    Recursion method factorial()

--- src/main/java/core/algorithms/recursion/Main.java
+++ src/main/java/core/algorithms/recursion/Main.java
@@ -2,7 +2,8 @@ package core.algorithms.recursion;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(countDown(5));
+//        System.out.println(countDown(5));
+        System.out.println(factorial(5));
     }
 
     public static int countDown(int i) {
@@ -10,5 +11,12 @@ public class Main {
         if (i <= 1) {
             return i;
         } else return countDown(i - 1);
+    }
+
+    public static int factorial(int x) {
+        System.out.println(x);
+        if (x == 1) {
+            return 1;
+        } else return x * factorial(x - 1);
     }
 }
