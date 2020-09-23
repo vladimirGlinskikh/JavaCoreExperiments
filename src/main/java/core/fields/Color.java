d826bcd24f07df073cf2fe48d2e3f7e53a99db7a
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-24 00:54:42 +0600

    Re-inheritance of fields

---
+++ src/main/java/core/fields/Color.java
@@ -0,0 +1,7 @@
+package core.fields;
+
+public interface Color {
+    int RED = 0,
+            GREEN = 1,
+            BLUE = 2;
+}
---
+++ src/main/java/core/fields/Colorable.java
@@ -0,0 +1,7 @@
+package core.fields;
+
+public interface Colorable {
+    int RED = 0xff0000,
+            GREEN = 0x00ff00,
+            BLUE = 0x0000ff;
+}
