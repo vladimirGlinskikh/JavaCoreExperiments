9f1e4d683052b092dcc9e0773785fd45ac3520ce
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-23 21:47:32 +0600

    Inheritance of class members with package access

--- src/main/java/core/superinterfaces/classMembers/Point.java
+++ src/main/java/core/superinterfaces/classMembers/Point.java
@@ -3,17 +3,27 @@ package core.superinterfaces.classMembers;
 public class Point {
     int x, y;
 
-    private Point() {
-        reset();
-    }
+//    private Point() {
+//        reset();
+//    }
 
     Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
     private void reset() {
         this.x = 0;
         this.y = 0;
+    }
+
+    public void move(int dx, int dy) {
+        x += dx;
+        y += dy;
     }
 }
