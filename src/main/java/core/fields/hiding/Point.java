0fb2478d446fd90ce00bcc5ab7e5ef6df7bee5bd
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-09-24 06:15:55 +0600

    Hiding of class variables

---
+++ src/main/java/core/fields/hiding/Point.java
@@ -0,0 +1,17 @@
+package core.fields.hiding;
+
+public class Point {
+    static int x = 2;
+}
+
+class Test extends Point {
+    static double x = 4.7;
+
+    public static void main(String[] args) {
+        new Test().printX();
+    }
+
+    void printX() {
+        System.out.println(x + " " + super.x);
+    }
+}
