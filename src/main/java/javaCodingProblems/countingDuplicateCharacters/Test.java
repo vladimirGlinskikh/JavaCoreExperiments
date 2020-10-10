e083f0de8f0616a90587f52cfb7b646d05b12380
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-10 22:42:15 +0600

    Another solution with stream feature

--- src/main/java/javaCodingProblems/countingDuplicateCharacters/Test.java
+++ src/main/java/javaCodingProblems/countingDuplicateCharacters/Test.java
@@ -2,18 +2,26 @@ package javaCodingProblems.countingDuplicateCharacters;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
         System.out.println(countDuplicateCharacters("I love Java"));
     }
 
-    public static Map<Character, Integer> countDuplicateCharacters(String string) {
-        Map<Character, Integer> result = new HashMap<>();
-        for (int i = 0; i < string.length(); i++) {
-            char ch = string.charAt(i);
-            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
-        }
+//    public static Map<Character, Integer> countDuplicateCharacters(String string) {
+//        Map<Character, Integer> result = new HashMap<>();
+//        for (int i = 0; i < string.length(); i++) {
+//            char ch = string.charAt(i);
+//            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+//        }
+//        return result;
+//    }
+
+    public static Map<Character, Long> countDuplicateCharacters(String string) {
+        Map<Character, Long> result = string.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
         return result;
     }
 }
