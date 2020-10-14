995d7943bee4bd99924c32fb84fe45b3e0eb96c2
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-14 23:15:34 +0600

    Another solution with functional style

--- src/main/java/javaCodingProblems/checkingWhetherAStringContainsOnlyDigits/Test.java
+++ src/main/java/javaCodingProblems/checkingWhetherAStringContainsOnlyDigits/Test.java
@@ -5,12 +5,17 @@ public class Test {
         System.out.println(containsOnlyDigits("12 I love Java!"));
     }
 
+//    public static boolean containsOnlyDigits(String string) {
+//        for (int i = 0; i < string.length(); i++) {
+//            if (!Character.isDigit(string.charAt(i))) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
     public static boolean containsOnlyDigits(String string) {
-        for (int i = 0; i < string.length(); i++) {
-            if (!Character.isDigit(string.charAt(i))) {
-                return false;
-            }
-        }
-        return true;
+        return !string.chars()
+                .anyMatch(n -> !Character.isDigit(n));
     }
 }
