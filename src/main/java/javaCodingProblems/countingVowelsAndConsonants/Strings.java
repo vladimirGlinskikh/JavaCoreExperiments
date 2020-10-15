72e15948da10d0c3265c9318641b11fc235505d2
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-15 17:03:44 +0600

    Another solution with functional style

--- src/main/java/javaCodingProblems/countingVowelsAndConsonants/Strings.java
+++ src/main/java/javaCodingProblems/countingVowelsAndConsonants/Strings.java
@@ -8,19 +8,33 @@ public class Strings {
 
     private static final Set<Character> allVowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
 
-    public static Pair<Integer, Integer> countVowelAndConsonants(String string) {
-        string = string.toLowerCase();
-        int vowels = 0;
-        int consonants = 0;
+//    public static Pair<Integer, Integer> countVowelAndConsonants(String string) {
+//        string = string.toLowerCase();
+//        int vowels = 0;
+//        int consonants = 0;
+//
+//        for (int i = 0; i < string.length(); i++) {
+//            char ch = string.charAt(i);
+//            if (allVowels.contains(ch)) {
+//                vowels++;
+//            } else if ((ch >= 'a' && ch <= 'z')) {
+//                consonants++;
+//            }
+//        }
+//        return Pair.of(vowels, consonants);
+//    }
 
-        for (int i = 0; i < string.length(); i++) {
-            char ch = string.charAt(i);
-            if (allVowels.contains(ch)) {
-                vowels++;
-            } else if ((ch >= 'a' && ch <= 'z')) {
-                consonants++;
-            }
-        }
+    public static Pair<Long, Long> countVowelAndConsonants(String string) {
+        string = string.toLowerCase();
+        long vowels = string.chars()
+                .filter(c -> allVowels.contains((char) c))
+                .count();
+
+        long consonants = string.chars()
+                .filter(c -> !allVowels.contains((char) c))
+                .filter(ch -> (ch >= 'a' && ch <= 'z'))
+                .count();
+
         return Pair.of(vowels, consonants);
     }
 }
