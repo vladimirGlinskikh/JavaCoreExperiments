5e7e25dcd042db7502316f54f62438990c7e91a6
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-17 21:45:48 +0600

    Another solution with Unicode surrogate pairs

--- src/main/java/javaCodingProblems/countingTheOccurrencesOfACertainCharacter/Test.java
+++ src/main/java/javaCodingProblems/countingTheOccurrencesOfACertainCharacter/Test.java
@@ -2,10 +2,19 @@ package javaCodingProblems.countingTheOccurrencesOfACertainCharacter;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(countOccurrencesOfACertainCharacter("I love Java!", 'a'));
+        System.out.println(countOccurrencesOfACertainCharacter("I love Java!", "a"));
     }
 
-    public static int countOccurrencesOfACertainCharacter(String string, char ch) {
-        return string.length() - string.replace(String.valueOf(ch), "").length();
+//    public static int countOccurrencesOfACertainCharacter(String string, char ch) {
+//        return string.length() - string.replace(String.valueOf(ch), "").length();
+//    }
+
+    public static int countOccurrencesOfACertainCharacter(String string, String ch) {
+        if (ch.codePointCount(0, ch.length()) > 1) {
+            return -1;
+        }
+
+        int result = string.length() - string.replace(ch, "").length();
+        return ch.length() == 2 ? result / 2 : result;
     }
 }
