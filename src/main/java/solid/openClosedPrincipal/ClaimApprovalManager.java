329819fb6da9b1c639b56c65a47d06aa78d63aa6
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-23 20:25:18 +0600

    modified the ClaimApprovalManager class by adding a new processVehicleClaim( ) method to incorporate a new functionality (this is a clear violation of the Open Closed principal)

--- src/main/java/solid/openClosedPrincipal/ClaimApprovalManager.java
+++ src/main/java/solid/openClosedPrincipal/ClaimApprovalManager.java
@@ -6,4 +6,10 @@ public class ClaimApprovalManager {
             System.out.println("ClaimApprovalManager: Valid claim. Currently processing claim for approval...");
         }
     }
+
+    public void processVehicleClaim(VehicleInsuranceSurveyor surveyor){
+        if (surveyor.isValidClaim()){
+            System.out.println("ClaimApprovalManager: Valid claim. Currently processing claim for approval...");
+        }
+    }
 }
---
+++ src/main/java/solid/openClosedPrincipal/VehicleInsuranceSurveyor.java
@@ -0,0 +1,8 @@
+package solid.openClosedPrincipal;
+
+public class VehicleInsuranceSurveyor {
+    public boolean isValidClaim() {
+        System.out.println("VehicleInsuranceSurveyor: Validating vehicle insurance claim...");
+        return true;
+    }
+}
