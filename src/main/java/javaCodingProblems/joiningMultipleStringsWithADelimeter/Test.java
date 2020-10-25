34d1bf1d4163a6c56b73335b4f0a4e81668fd3de
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-25 20:54:51 +0600

    This solution made with StringBuilder

---
+++ src/main/java/javaCodingProblems/joiningMultipleStringsWithADelimeter/Test.java
@@ -0,0 +1,17 @@
+package javaCodingProblems.joiningMultipleStringsWithADelimeter;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(joinByDelimiter(' ', "I", "love", "Java", "!"));
+    }
+
+    public static String joinByDelimiter(char delimiter, String... args) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        for (i = 0; i < args.length - 1; i++) {
+            result.append(args[i]).append(delimiter);
+        }
+        result.append(args[i]);
+        return result.toString();
+    }
+}
