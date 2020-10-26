98ed279e347e258306c7174c7cae2997e0d743fd
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-26 20:04:17 +0600

    Another solution with functional style

--- src/main/java/javaCodingProblems/generatingAllPermutations/Test.java
+++ src/main/java/javaCodingProblems/generatingAllPermutations/Test.java
@@ -2,11 +2,13 @@ package javaCodingProblems.generatingAllPermutations;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class Test {
     public static void main(String[] args) {
 //        permuteAndPrint("", "abc");
-        System.out.println(permuteAndStore("", "Java"));
+//        System.out.println(permuteAndStore("", "Java"));
+        permuteAndPrintStream("", "Java");
     }
 
 //    public static void permuteAndPrint(String prefix, String string) {
@@ -21,18 +23,30 @@ public class Test {
 //        }
 //    }
 
-    public static Set<String> permuteAndStore(String prefix, String string) {
-        Set<String> permutations = new HashSet<>();
-        int n = string.length();
+//    public static Set<String> permuteAndStore(String prefix, String string) {
+//        Set<String> permutations = new HashSet<>();
+//        int n = string.length();
+//
+//        if (n == 0) {
+//            permutations.add(prefix);
+//        } else {
+//            for (int i = 0; i < n; i++) {
+//                permutations.addAll(permuteAndStore(prefix + string.charAt(i),
+//                        string.substring(i + 1, n) + string.substring(0, i)));
+//            }
+//        }
+//        return permutations;
+//    }
 
+    public static void permuteAndPrintStream(String prefix, String string) {
+        int n = string.length();
         if (n == 0) {
-            permutations.add(prefix);
+            System.out.print(prefix + " ");
         } else {
-            for (int i = 0; i < n; i++) {
-                permutations.addAll(permuteAndStore(prefix + string.charAt(i),
-                        string.substring(i + 1, n) + string.substring(0, i)));
-            }
+            IntStream.range(0, n)
+                    .parallel()
+                    .forEach(i -> permuteAndPrintStream(prefix + string.charAt(i),
+                            string.substring(i + 1, n) + string.substring(0, i)));
         }
-        return permutations;
     }
 }
