a013033442107dc14009a0afb7278c71cb6faec3
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-26 19:58:41 +0600

    Another solution with Set since it eliminates duplicates

--- src/main/java/javaCodingProblems/generatingAllPermutations/Test.java
+++ src/main/java/javaCodingProblems/generatingAllPermutations/Test.java
@@ -1,19 +1,38 @@
 package javaCodingProblems.generatingAllPermutations;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test {
     public static void main(String[] args) {
-        permuteAndPrint("", "abc");
+//        permuteAndPrint("", "abc");
+        System.out.println(permuteAndStore("", "Java"));
     }
 
-    public static void permuteAndPrint(String prefix, String string) {
+//    public static void permuteAndPrint(String prefix, String string) {
+//        int n = string.length();
+//        if (n == 0) {
+//            System.out.print(prefix + " ");
+//        } else {
+//            for (int i = 0; i < n; i++) {
+//                permuteAndPrint(prefix + string.charAt(i),
+//                        string.substring(i + 1, n) + string.substring(0, i));
+//            }
+//        }
+//    }
+
+    public static Set<String> permuteAndStore(String prefix, String string) {
+        Set<String> permutations = new HashSet<>();
         int n = string.length();
+
         if (n == 0) {
-            System.out.print(prefix + " ");
+            permutations.add(prefix);
         } else {
             for (int i = 0; i < n; i++) {
-                permuteAndPrint(prefix + string.charAt(i),
-                        string.substring(i + 1, n) + string.substring(0, i));
+                permutations.addAll(permuteAndStore(prefix + string.charAt(i),
+                        string.substring(i + 1, n) + string.substring(0, i)));
             }
         }
+        return permutations;
     }
 }
