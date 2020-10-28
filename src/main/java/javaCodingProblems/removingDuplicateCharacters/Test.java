d12d0d7a317f0b5e7163540374d9a81818f2a6dd
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-28 23:06:09 +0600

    This solution relies on a collaboration between HashSet and StringBuilder

--- src/main/java/javaCodingProblems/removingDuplicateCharacters/Test.java
+++ src/main/java/javaCodingProblems/removingDuplicateCharacters/Test.java
@@ -1,17 +1,33 @@
 package javaCodingProblems.removingDuplicateCharacters;
 
+import com.sun.source.doctree.SeeTree;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println(removeDuplicates("I love Java!"));
     }
 
     public static String removeDuplicates(String string) {
+//        char[] chArray = string.toCharArray();
+//        StringBuilder stringBuilder = new StringBuilder();
+//
+//        for (char ch : chArray) {
+//            if (stringBuilder.indexOf(String.valueOf(ch)) == -1) {
+//                stringBuilder.append(ch);
+//            }
+//        }
+//        return stringBuilder.toString();
+
         char[] chArray = string.toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
+        Set<Character> chHashSet = new HashSet<>();
 
-        for (char ch : chArray) {
-            if (stringBuilder.indexOf(String.valueOf(ch)) == -1) {
-                stringBuilder.append(ch);
+        for (char c : chArray) {
+            if (chHashSet.add(c)) {
+                stringBuilder.append(c);
             }
         }
         return stringBuilder.toString();
