4e35b4328216e6e8cad4c965030861e4ad881c4b
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-28 23:07:43 +0600

    This solution made with functional style

--- src/main/java/javaCodingProblems/removingDuplicateCharacters/Test.java
+++ src/main/java/javaCodingProblems/removingDuplicateCharacters/Test.java
@@ -2,8 +2,10 @@ package javaCodingProblems.removingDuplicateCharacters;
 
 import com.sun.source.doctree.SeeTree;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
@@ -21,15 +23,19 @@ public class Test {
 //        }
 //        return stringBuilder.toString();
 
-        char[] chArray = string.toCharArray();
-        StringBuilder stringBuilder = new StringBuilder();
-        Set<Character> chHashSet = new HashSet<>();
+//        char[] chArray = string.toCharArray();
+//        StringBuilder stringBuilder = new StringBuilder();
+//        Set<Character> chHashSet = new HashSet<>();
+//
+//        for (char c : chArray) {
+//            if (chHashSet.add(c)) {
+//                stringBuilder.append(c);
+//            }
+//        }
+//        return stringBuilder.toString();
 
-        for (char c : chArray) {
-            if (chHashSet.add(c)) {
-                stringBuilder.append(c);
-            }
-        }
-        return stringBuilder.toString();
+        return Arrays.asList(string.split("")).stream()
+                .distinct()
+                .collect(Collectors.joining());
     }
 }
