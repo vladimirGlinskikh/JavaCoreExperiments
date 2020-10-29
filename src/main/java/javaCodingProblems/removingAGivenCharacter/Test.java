8e091ba7560b96f5203759c71eb30160618f9a37
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-10-29 23:47:25 +0600

    If we want to remove a Unicode surrogate pair, then we can rely on codePointAt() and codePoints()

--- src/main/java/javaCodingProblems/removingAGivenCharacter/Test.java
+++ src/main/java/javaCodingProblems/removingAGivenCharacter/Test.java
@@ -5,25 +5,34 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(removeCharacter("I love Java!", 'a'));
+        System.out.println(removeCharacter("I love Java!", "l"));
     }
 
-    public static String removeCharacter(String string, char ch) {
-//        return string.replaceAll(Pattern.quote(String.valueOf(ch)), "");
-
-//        StringBuilder stringBuilder = new StringBuilder();
-//        char[] chArray = string.toCharArray();
+//    public static String removeCharacter(String string, char ch) {
+////        return string.replaceAll(Pattern.quote(String.valueOf(ch)), "");
 //
-//        for (char c : chArray) {
-//            if (c != ch) {
-//                stringBuilder.append(c);
-//            }
-//        }
-//        return stringBuilder.toString();
+////        StringBuilder stringBuilder = new StringBuilder();
+////        char[] chArray = string.toCharArray();
+////
+////        for (char c : chArray) {
+////            if (c != ch) {
+////                stringBuilder.append(c);
+////            }
+////        }
+////        return stringBuilder.toString();
+//
+//        return string.chars()
+//                .filter(c -> c != ch)
+//                .mapToObj(c -> String.valueOf((char) c))
+//                .collect(Collectors.joining());
+//    }
 
-        return string.chars()
-                .filter(c -> c != ch)
-                .mapToObj(c -> String.valueOf((char) c))
+    public static String removeCharacter(String string, String ch) {
+        int codePoint = ch.codePointAt(0);
+
+        return string.codePoints()
+                .filter(c -> c != codePoint)
+                .mapToObj(c -> String.valueOf(Character.toChars(c)))
                 .collect(Collectors.joining());
     }
 }
