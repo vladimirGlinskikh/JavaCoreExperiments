26b6e20ffcf3e8cb3b9703fdd294a77374ba0783
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-11-02 22:02:07 +0600

    An interface to define the behaviors of toys

---
+++ src/main/java/solid/interfaceSegregationPrinciple/Toy.java
@@ -0,0 +1,11 @@
+package solid.interfaceSegregationPrinciple;
+
+public interface Toy {
+    void setPrice(double price);
+
+    void setColor(String color);
+
+    void move();
+
+    void fly();
+}
