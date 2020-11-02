ab3df829af7a5ac38d6a03b5ea933e9ed6c3ba2c
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-11-03 05:39:53 +0600

    Dependency inversion principal violation (bad example)

---
+++ src/main/java/solid/dependencyInversionPrinciple/LightBulb.java
@@ -0,0 +1,11 @@
+package solid.dependencyInversionPrinciple;
+
+public class LightBulb {
+    public void turnOn() {
+        System.out.println("LightBulb: Bulb turned on...");
+    }
+
+    public void turnOff() {
+        System.out.println("LightBulb: Bulb turned off...");
+    }
+}
