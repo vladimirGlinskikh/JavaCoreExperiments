4bd09d2be8075c326c74344b320e2e5605be7eed
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-03 10:19:51 +0600

    Another solutions use with method contains()

--- src/main/java/javaCodingProblems/checkingThatAStringContainsASubstring/Test.java
+++ src/main/java/javaCodingProblems/checkingThatAStringContainsASubstring/Test.java
@@ -4,10 +4,12 @@ public class Test {
     public static void main(String[] args) {
         String text = "I love Java!";
         String subtext = "av";
-        System.out.println(contains(text, subtext));
+//        System.out.println(contains(text, subtext));
+        boolean contains = text.contains(subtext);
+        System.out.println(contains);
     }
 
-    public static boolean contains(String text, String subtext) {
-        return text.indexOf(subtext) != -1;
-    }
+//    public static boolean contains(String text, String subtext) {
+//        return text.indexOf(subtext) != -1;
+//    }
 }
