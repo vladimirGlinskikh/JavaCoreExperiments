fd77ca6c0875f6d62a56fa9f67db89f25462d41b
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-10 23:04:59 +0600

    Convert String with parseInt() method

---
+++ src/main/java/core/string/StringToInt/StringToIntConverter.java
@@ -0,0 +1,8 @@
+package core.string.StringToInt;
+
+public class StringToIntConverter {
+    public int convertWithParseInt(String string) {
+        int num = Integer.parseInt(string);
+        return num;
+    }
+}
---
+++ src/test/java/core/string/StringToInt/StringToIntConverterTest.java
@@ -0,0 +1,30 @@
+package core.string.StringToInt;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringToIntConverterTest {
+    private StringToIntConverter stringToIntConverter;
+    String string;
+
+    @BeforeEach
+    void setUp() {
+        string = "743";
+        stringToIntConverter = new StringToIntConverter();
+    }
+
+    @AfterEach
+    void tearDown() {
+        string = null;
+    }
+
+    @Test
+    void convertWithParseInt() {
+        int val = stringToIntConverter.convertWithParseInt(string);
+        System.out.println(val);
+        assertEquals(val, 743);
+    }
+}
