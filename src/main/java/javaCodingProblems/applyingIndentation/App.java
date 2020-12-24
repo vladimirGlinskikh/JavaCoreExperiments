2fa94282182ae3a9e431d36e2c482c94660dedc0
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-24 21:22:18 +0600

    Cascade indentation

--- src/main/java/javaCodingProblems/applyingIndentation/App.java
+++ src/main/java/javaCodingProblems/applyingIndentation/App.java
@@ -1,15 +1,23 @@
 package javaCodingProblems.applyingIndentation;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class App {
-    public static String days = "Sunday\n"
-            + "Monday\n"
-            + "Tuesday\n"
-            + "Wednesday\n"
-            + "Thursday\n"
-            + "Friday\n"
-            + "Saturday";
+    //    public static String days = "Sunday\n"
+//            + "Monday\n"
+//            + "Tuesday\n"
+//            + "Wednesday\n"
+//            + "Thursday\n"
+//            + "Friday\n"
+//            + "Saturday";
+    public static List<String> days = Arrays.asList("Sunday", "Monday", "Tuesday",
+            "Wednesday", "Thursday", "Friday", "Saturday");
 
     public static void main(String[] args) {
-        System.out.println(days.indent(10));
+//        System.out.println(days.indent(10));
+        for (int i = 0; i < days.size(); i++) {
+            System.out.print(days.get(i).indent(i));
+        }
     }
 }
