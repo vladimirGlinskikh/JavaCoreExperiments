a88057f1c032f48864401a9c97d28769216ba3aa
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-27 02:08:25 +0600

    Transforming string with transform() method

--- src/main/java/javaCodingProblems/transformingStrings/TransformingString.java
+++ src/main/java/javaCodingProblems/transformingStrings/TransformingString.java
@@ -4,12 +4,19 @@ import java.util.stream.Stream;
 
 public class TransformingString {
     public static void main(String[] args) {
-        System.out.println(resultMap);
+        System.out.println(result);
     }
-    public static String resultMap = Stream.of("hello ")
-//            .map(s -> s + " Java!")
-            .map(String::toUpperCase)
-            .map(s -> s.repeat(3))
-            .findFirst()
-            .get();
+//    public static String resultMap = Stream.of("hello")
+//            .map(s -> s + " Java! ")
+//            .map(String::toUpperCase)
+//            .map(s -> s.repeat(3))
+//            .findFirst()
+//            .get();
+
+    public static String result = "hello"
+            .transform(s -> s + " java! ")
+            .transform(String::toUpperCase)
+            .transform(s -> s.repeat(2));
+
+
 }
