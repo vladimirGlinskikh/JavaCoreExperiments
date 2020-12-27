ffc92b0b7112c4b891a18f593cecb6e250fbd256
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-27 09:52:16 +0600

    Summing two large int/long values and operation overflow

---
+++ src/main/java/javaCodingProblems/summingTwoLargeIntLongValuesAndOperationOverflow/App.java
@@ -0,0 +1,21 @@
+package javaCodingProblems.summingTwoLargeIntLongValuesAndOperationOverflow;
+
+public class App {
+    public static void main(String[] args) {
+        int x = Integer.MAX_VALUE;
+        int y = Integer.MAX_VALUE;
+        int z = x + y;
+        System.out.println(z);
+        System.out.println("-----------------");
+        long x1 = Integer.MAX_VALUE;
+        long y1 = Integer.MAX_VALUE;
+        long z1 = x1 + y1;
+        System.out.println(z1);
+        System.out.println("-------------------");
+        long x2 = Long.MAX_VALUE;
+        long y2 = Long.MAX_VALUE;
+        long z2 = Long.sum(x2, y2);
+        System.out.println(z2);
+        System.out.println("--------------------");
+    }
+}
