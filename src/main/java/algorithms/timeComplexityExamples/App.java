6b30070fa0e6fd511afbdab6ef66409a1a57bed3
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2021-01-05 22:00:21 +0600

    Time Complexity: O(n2), two nested for loop takes quadratic time

--- src/main/java/algorithms/timeComplexityExamples/App.java
+++ src/main/java/algorithms/timeComplexityExamples/App.java
@@ -1,24 +1,20 @@
 package algorithms.timeComplexityExamples;
 
-import java.util.concurrent.TimeUnit;
-
 public class App {
     public static void main(String[] args) throws InterruptedException {
-        long start = System.nanoTime();
-        System.out.println(fun1(1_000_000));
-        long end = System.nanoTime();
+        long start = System.currentTimeMillis();
+        fun1(1_000_000);
+        long end = System.currentTimeMillis();
         long sum = end - start;
         System.out.println(sum);
-
-        long convert = TimeUnit.SECONDS.convert(sum, TimeUnit.NANOSECONDS);
-
-        System.out.println(convert + " seconds");
     }
 
     public static int fun1(int n) {
-        int m = 0;
-        for (int i = 0; i < n; i++) {
-            m += 1;
+        int i, j, m = 0;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                m += 1;
+            }
         }
         return m;
     }
