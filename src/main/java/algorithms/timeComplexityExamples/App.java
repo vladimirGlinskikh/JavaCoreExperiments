bccfa29a4cc7d9da526b9be7ea57ac4bed1fc7d2
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2021-01-05 22:03:34 +0600

    Time Complexity: O(N+(N-1)+(N-2)+...) == O(N(N+1)/2) == O(n2)

--- src/main/java/algorithms/timeComplexityExamples/App.java
+++ src/main/java/algorithms/timeComplexityExamples/App.java
@@ -3,16 +3,16 @@ package algorithms.timeComplexityExamples;
 public class App {
     public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
-        fun1(1_000_000);
+        System.out.println(fun1(1_000));
         long end = System.currentTimeMillis();
         long sum = end - start;
-        System.out.println(sum);
+        System.out.println(sum + " milliseconds");
     }
 
     public static int fun1(int n) {
         int i, j, m = 0;
         for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
+            for (j = 0; j < i; j++) {
                 m += 1;
             }
         }
