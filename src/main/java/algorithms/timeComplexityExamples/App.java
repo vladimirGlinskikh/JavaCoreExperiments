b585ff2e0c33f6a1ee9ceb959634361201cc10f7
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2021-01-05 22:06:14 +0600

    Time Complexity: O(log(n))

--- src/main/java/algorithms/timeComplexityExamples/App.java
+++ src/main/java/algorithms/timeComplexityExamples/App.java
@@ -3,18 +3,18 @@ package algorithms.timeComplexityExamples;
 public class App {
     public static void main(String[] args) throws InterruptedException {
         long start = System.currentTimeMillis();
-        System.out.println(fun1(1_000));
+        System.out.println(fun1(1_000_000));
         long end = System.currentTimeMillis();
         long sum = end - start;
         System.out.println(sum + " milliseconds");
     }
 
     public static int fun1(int n) {
-        int i, j, m = 0;
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < i; j++) {
-                m += 1;
-            }
+        int i, m = 0;
+        i = 1;
+        while (i < n) {
+            m += 1;
+            i = i * 2;
         }
         return m;
     }
