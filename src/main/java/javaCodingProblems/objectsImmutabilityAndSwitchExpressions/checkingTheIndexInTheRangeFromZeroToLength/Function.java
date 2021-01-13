59b482c36d0a1b976cba8a6456d8d4bde77b3ed1
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-13 23:30:18 +0600

    Ranges can be imposed in code via to use the Objects.checkIndex() method

--- src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheIndexInTheRangeFromZeroToLength/Function.java
+++ src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheIndexInTheRangeFromZeroToLength/Function.java
@@ -1,28 +1,33 @@
 package javaCodingProblems.objectsImmutabilityAndSwitchExpressions.checkingTheIndexInTheRangeFromZeroToLength;
 
+import java.util.Objects;
+
 public class Function {
     private final int X_UPPER_BOUND = 11;
     private final int Y_UPPER_BOUND = 16;
     private final int x;
 
     public Function(int x) {
-        if (x < 0 || x >= X_UPPER_BOUND) {
-            throw new IndexOutOfBoundsException("...");
-        }
-        this.x = x;
+//        if (x < 0 || x >= X_UPPER_BOUND) {
+//            throw new IndexOutOfBoundsException("...");
+//        }
+//        this.x = x;
+        this.x = Objects.checkIndex(x, X_UPPER_BOUND);
     }
 
     public int xMinusY(int y) {
-        if (y < 0 || y >= x) {
-            throw new IndexOutOfBoundsException("...");
-        }
+//        if (y < 0 || y >= x) {
+//            throw new IndexOutOfBoundsException("...");
+//        }
+        Objects.checkIndex(y, x);
         return x - y;
     }
 
     public int oneMinusY(int y) {
-        if (y < 0 || y >= Y_UPPER_BOUND) {
-            throw new IndexOutOfBoundsException("...");
-        }
+//        if (y < 0 || y >= Y_UPPER_BOUND) {
+//            throw new IndexOutOfBoundsException("...");
+//        }
+        Objects.checkIndex(y, Y_UPPER_BOUND);
         return 1 - y;
     }
 }
