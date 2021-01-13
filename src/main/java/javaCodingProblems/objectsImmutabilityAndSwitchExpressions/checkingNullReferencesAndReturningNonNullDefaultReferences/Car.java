5a99e1ee14d0517dee3a60231d6decf6bc32301e
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-13 23:05:50 +0600

    A solution to this problem can be simplified via two methods from Objects class

--- src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingNullReferencesAndReturningNonNullDefaultReferences/Car.java
+++ src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingNullReferencesAndReturningNonNullDefaultReferences/Car.java
@@ -1,22 +1,26 @@
 package javaCodingProblems.objectsImmutabilityAndSwitchExpressions.checkingNullReferencesAndReturningNonNullDefaultReferences;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class Car {
     private final String name;
     private final Color color;
 
     public Car(String name, Color color) {
-        if (name == null) {
-            this.name = "No name";
-        } else {
-            this.name = name;
-        }
+//        if (name == null) {
+//            this.name = "No name";
+//        } else {
+//            this.name = name;
+//        }
+//
+//        if (color == null) {
+//            this.color = new Color(0, 0, 0);
+//        } else {
+//            this.color = color;
+//        }
 
-        if (color == null) {
-            this.color = new Color(0, 0, 0);
-        } else {
-            this.color = color;
-        }
+        this.name = Objects.requireNonNullElse(name, "No name");
+        this.color = Objects.requireNonNullElseGet(color, () -> new Color(0, 0, 0));
     }
 }
