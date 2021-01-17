81ea6516f461452392b978ce93c4b20be4741316
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-18 03:17:03 +0600

    This solution with equals and hashCode by @Override

--- src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/equalsAndHashCode/Player.java
+++ src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/equalsAndHashCode/Player.java
@@ -1,5 +1,7 @@
 package javaCodingProblems.objectsImmutabilityAndSwitchExpressions.equalsAndHashCode;
 
+import java.util.Objects;
+
 public class Player {
     private int id;
     private String name;
@@ -7,5 +9,32 @@ public class Player {
     public Player(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Player p1 = new Player(1, "Vl");
+        Player p2 = new Player(1, "Vl");
+        System.out.println(p1.equals(p2));
     }
 }
