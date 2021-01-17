4b07fc5c003b6c2fb66fc2ee1dbdb06cfd71bb9d
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-17 23:04:03 +0600

    Based on the previous problem, the condition for n can be replaced with Objects.checkIndex() methods

--- src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheSubrangeInTheRangeFromZeroToLength/Function.java
+++ src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheSubrangeInTheRangeFromZeroToLength/Function.java
@@ -1,20 +1,30 @@
 package javaCodingProblems.objectsImmutabilityAndSwitchExpressions.checkingTheSubrangeInTheRangeFromZeroToLength;
 
+import java.util.Objects;
+
 public class Function {
     private static final int N_UPPER_BOUND = 101;
     private final int n;
 
     public Function(int n) {
-        if (n < 0 || n >= N_UPPER_BOUND) {
-            throw new IndexOutOfBoundsException("...");
-        }
-        this.n = n;
+//        if (n < 0 || n >= N_UPPER_BOUND) {
+//            throw new IndexOutOfBoundsException("...");
+//        }
+//        this.n = n;
+        this.n = Objects.checkIndex(n, N_UPPER_BOUND);
     }
 
     public int yMinusX(int x, int y) {
-        if (x < 0 || x > y || y >= n) {
-            throw new IndexOutOfBoundsException("...");
-        }
+//        if (x < 0 || x > y || y >= n) {
+//            throw new IndexOutOfBoundsException("...");
+//        }
+        Objects.checkFromToIndex(x, y, n);
         return y - x;
+    }
+
+    public static void main(String[] args) {
+        Function function = new Function(40);
+        int res = function.yMinusX(10, 40);
+        System.out.println(res);
     }
 }
