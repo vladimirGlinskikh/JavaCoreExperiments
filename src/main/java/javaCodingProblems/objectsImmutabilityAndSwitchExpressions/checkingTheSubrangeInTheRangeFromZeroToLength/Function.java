df85044fdd5886ae15a663b23c09945b180f193b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-17 22:50:57 +0600

    Begins will look as follow

---
+++ src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheSubrangeInTheRangeFromZeroToLength/Function.java
@@ -0,0 +1,13 @@
+package javaCodingProblems.objectsImmutabilityAndSwitchExpressions.checkingTheSubrangeInTheRangeFromZeroToLength;
+
+public class Function {
+    private final int n;
+
+    public Function(int n) {
+        this.n = n;
+    }
+
+    public int yMinusX(int x, int y) {
+        return y - x;
+    }
+}
