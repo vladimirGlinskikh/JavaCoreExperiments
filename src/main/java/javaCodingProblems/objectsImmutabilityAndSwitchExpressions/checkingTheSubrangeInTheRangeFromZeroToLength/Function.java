745dcc07365d45761faaf5e6f119003be87a2337
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-01-17 22:55:05 +0600

    Let's impose the following ranges

--- src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheSubrangeInTheRangeFromZeroToLength/Function.java
+++ src/main/java/javaCodingProblems/objectsImmutabilityAndSwitchExpressions/checkingTheSubrangeInTheRangeFromZeroToLength/Function.java
@@ -1,13 +1,20 @@
 package javaCodingProblems.objectsImmutabilityAndSwitchExpressions.checkingTheSubrangeInTheRangeFromZeroToLength;
 
 public class Function {
+    private static final int N_UPPER_BOUND = 101;
     private final int n;
 
     public Function(int n) {
+        if (n < 0 || n >= N_UPPER_BOUND) {
+            throw new IndexOutOfBoundsException("...");
+        }
         this.n = n;
     }
 
     public int yMinusX(int x, int y) {
+        if (x < 0 || x > y || y >= n) {
+            throw new IndexOutOfBoundsException("...");
+        }
         return y - x;
     }
 }
