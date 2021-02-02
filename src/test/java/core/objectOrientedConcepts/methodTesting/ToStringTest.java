3aae31fc0e458b61a1cf6b4eeba975694b0a403f
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-02-02 23:59:33 +0600

    Refactoring test

--- src/test/java/core/objectOrientedConcepts/methodTesting/ToStringTest.java
+++ src/test/java/core/objectOrientedConcepts/methodTesting/ToStringTest.java
@@ -1,30 +1,36 @@
 package core.objectOrientedConcepts.methodTesting;
 
 import core.objectOrientedConcepts.dataAbstraction.anInterfaceAndAClassThatImplementsTheInterface.FullTimeEmployee;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ToStringTest {
-    protected FullTimeEmployee full;
-    protected String expected;
+    private FullTimeEmployee full;
+    private String expected;
+
+    @BeforeEach
+    void setUp() {
+        full = new FullTimeEmployee("a", 150.00);
+        full = new FullTimeEmployee("b", 356.78);
+    }
+
 
     @Test
-    public void toStringTest(){
-        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee("a", 150.00);
-        String expected = "a $150.00 FULL TIME";
-        assertNotEquals(expected, fullTimeEmployee.toString());
+    public void toStringTest() {
+        expected = "a $150.00 FULL TIME";
+        assertNotEquals(expected, full.toString());
     }
 
     @Test
-    public void toStringTest1(){
-        full = new FullTimeEmployee("b", 356.78);
+    public void toStringTest1() {
         expected = "b $356.78 HOURLY";
         assertEquals(expected, full.toString());
     }
 
     @Test
-    public void toStringTest2(){
+    public void toStringTest2() {
         full = new FullTimeEmployee();
         expected = " $0.00 HOURLY";
         assertEquals(expected, full.toString());
