e4f0b5cabf9d72c032759fd97b572a2b34b73482
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-03-04 17:03:40 +0600

    Answer: C Throwable

---
+++ src/main/java/OCP/practiceTest1/test8/Error.java
@@ -0,0 +1,11 @@
+package OCP.practiceTest1.test8;
+
+public class Error extends Throwable {
+}
+
+        /*A. RuntimeException
+        B. Exception
+        C. Throwable*/
+
+        /*Reason :
+        An Error is a subclass of Throwable class.*/
