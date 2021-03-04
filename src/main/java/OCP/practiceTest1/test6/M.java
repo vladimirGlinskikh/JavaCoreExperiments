7f600ca0f57740f502df4bc2e057844ca0c2c48c
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-03-04 16:11:57 +0600

    Answer: B Only two options

---
+++ src/main/java/OCP/practiceTest1/test6/M.java
@@ -0,0 +1,4 @@
+package OCP.practiceTest1.test6;
+
+public class M {
+}
---
+++ src/main/java/OCP/practiceTest1/test6/N.java
@@ -0,0 +1,28 @@
+package OCP.practiceTest1.test6;
+
+class N {
+}
+
+    /*Following options are available to fill the above blanks:
+        1. public
+        2. private
+        3. protected
+        4. final
+        5. abstract*/
+
+    /*How many above options can be used to fill above blank
+        (separately and not together) such that there is no compilation error?
+
+        A. Only one option
+        B. Only two options
+        C. Only three options
+        D. Only four options
+        E. All five options*/
+
+       /* Reason :
+        Top-level class can use only two access modifiers [public and
+        default(don't specify anything)]. private and protected cannot be
+        used.
+        As file name is M.java, hence class N cannot be public .
+        Top-level class can be final, hence it is a correct option.
+        Top-level class can be abstract and hence it is also a correct option.*/
