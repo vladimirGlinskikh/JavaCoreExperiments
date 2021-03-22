fb7266f4d49544d693a84a39757550ec3422d113
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-03-22 14:03:42 +0600

    ArrayList example with methods addAll(), indexOf(), lastIndexOf(), size(), contains(), toString(), clear(), isEmpty()

--- src/main/java/javaExperiments/collections/ArrayList/ArrayListMethods.java
+++ src/main/java/javaExperiments/collections/ArrayList/ArrayListMethods.java
@@ -11,7 +11,7 @@ public class ArrayListMethods {
         arrayList.add("one");
         arrayList.add("two");
         arrayList.add("three");
-        arrayList.add(1,"four");
+        arrayList.add(1, "four");
         System.out.println("add: " + arrayList);
 
 
@@ -21,16 +21,51 @@ public class ArrayListMethods {
         System.out.println("---------------------");
         arrayList.set(1, "five");
         System.out.print("set: ");
-        for (String s : arrayList){
+        for (String s : arrayList) {
             System.out.print(s + " ");
         }
 
-        System.out.println("\n" +"---------------------");
+        System.out.println("\n" + "---------------------");
         arrayList.remove(2);
         System.out.print("remove: ");
-        for (String s : arrayList){
+        for (String s : arrayList) {
             System.out.print(s + " ");
         }
         System.out.println("\n" + "---------------------");
+
+        List<String> list = new ArrayList<>();
+        list.add("list1");
+        list.add("list2");
+        list.add("list3");
+        list.add("list4");
+        list.add("list3");
+        list.addAll(2, arrayList);
+        System.out.print("addAll: ");
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+        System.out.println("\n" + "---------------------");
+
+        System.out.println("indexOf: " + list.indexOf("list3"));
+        System.out.println("--------------------------");
+
+        System.out.println("lastIndexOf: " + list.lastIndexOf("list3"));
+        System.out.println("--------------------------");
+
+        System.out.println("size: " + list.size());
+        System.out.println("--------------------------");
+
+        System.out.println("contains: " + list.contains("one"));
+        System.out.println("--------------------------");
+
+        System.out.println("toString: " + list.toString());
+        System.out.println("--------------------------");
+
+        list.clear();
+        System.out.println("clear: " + list);
+        System.out.println("------------------------");
+
+        System.out.println("isEmpty: " + list.isEmpty());
+        System.out.println("--------------------------");
     }
 }
