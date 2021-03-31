048816880c47264e8c751a1b92844fb8ac50c72c
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-03-31 19:32:00 +0600

    Stream.filter() example

---
+++ src/main/java/javaExperiments/streams/filter/App.java
@@ -0,0 +1,24 @@
+package javaExperiments.streams.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) {
+        Student student1 = new Student("Vladimir", 'm', 38, 3, 9.4);
+        Student student2 = new Student("Svetlana", 'f', 35, 4, 9.1);
+        Student student3 = new Student("Nikolay", 'm', 37, 2, 8.1);
+        Student student4 = new Student("Elena", 'f', 32, 1, 7.9);
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+
+        studentList = studentList.stream()
+                .filter(element -> element.getAge() > 25 && element.getAvgGrade() <= 9.5)
+                .collect(Collectors.toList());
+        System.out.println(studentList);
+    }
+}
---
+++ src/main/java/javaExperiments/streams/filter/Student.java
@@ -0,0 +1,33 @@
+package javaExperiments.streams.filter;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+    String name;
+    char sex;
+    int age;
+    int course;
+    double avgGrade;
+
+    public Student(String name, char sex, int age, int course, double avgGrade) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.avgGrade = avgGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", course=" + course +
+                ", avgGrade=" + avgGrade +
+                '}';
+    }
+}
