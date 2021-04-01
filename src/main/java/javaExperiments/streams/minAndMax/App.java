e862e31f05a2485f37b941209aeca58c8ad55244
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-01 11:48:12 +0600

    Stream min() and max() methods example

---
+++ src/main/java/javaExperiments/streams/minAndMax/App.java
@@ -0,0 +1,29 @@
+package javaExperiments.streams.minAndMax;
+
+import javaExperiments.streams.filter.Student;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Student student1 = new Student("Vladimir", 'm', 38, 3, 9.4);
+        Student student2 = new Student("Svetlana", 'f', 35, 4, 9.1);
+        Student student3 = new Student("Nikolay", 'm', 37, 2, 8.1);
+        Student student4 = new Student("Elena", 'f', 32, 1, 7.9);
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+
+//        Student student = studentList.stream()
+//                .min(Comparator.comparingInt(Student::getAge)).get();
+//        System.out.println(student);
+
+        Student student = studentList.stream()
+                .max(Comparator.comparingInt(Student::getAge)).get();
+        System.out.println(student);
+    }
+}
