920f0d9689a9898395931c642797a56bf1371f9f
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-03 11:16:34 +0600

    Stream limit() example

---
+++ src/main/java/javaExperiments/streams/limit/App.java
@@ -0,0 +1,25 @@
+package javaExperiments.streams.limit;
+
+import javaExperiments.streams.filter.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        Student student1 = new Student("Vladimir", 'm', 38, 3, 9.4);
+        Student student2 = new Student("Svetlana", 'f', 35, 4, 9.1);
+        Student student3 = new Student("Nikolay", 'm', 37, 2, 8.1);
+        Student student4 = new Student("Elena", 'f', 32, 1, 7.9);
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+
+        studentList.stream()
+                .filter(element -> element.getAge() > 35)
+                .limit(1)
+                .forEach(System.out::println);
+    }
+}
