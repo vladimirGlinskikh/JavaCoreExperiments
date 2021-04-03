b15c0f52becbeeb154c982c06e5f30f0c7076ce2
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-03 20:23:51 +0600

    Question is check

---
+++ src/main/java/javaRushAnswers/objects/Objects.java
@@ -0,0 +1,13 @@
+package javaRushAnswers.objects;
+
+public class Objects {
+   public static String okr = String.format("%.2f", p());
+
+    static double p() {
+        return 9.999999;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(okr);
+    }
+}
