f5d394614adc2f82167a860c78edce74c5e9ece3
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-04 22:13:35 +0600

    Methods example Thread class

--- src/main/java/javaExperiments/multithreading/thread/App.java
+++ src/main/java/javaExperiments/multithreading/thread/App.java
@@ -2,9 +2,21 @@ package javaExperiments.multithreading.thread;
 
 public class App {
     public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-        MyThreadOne myThreadOne = new MyThreadOne();
-        myThread.start();
-        myThreadOne.start();
+//        MyThread myThread = new MyThread();
+//        MyThreadOne myThreadOne = new MyThreadOne();
+//        myThread.start();
+//        myThreadOne.start();
+
+        MyThreadTwo myThreadTwo = new MyThreadTwo();
+        myThreadTwo.setName("my_thread");
+        myThreadTwo.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Name of myTreadTwo = " + myThreadTwo.getName() + "\n" +
+                "Priority of myThreadTwo = " + myThreadTwo.getPriority());
+
+        System.out.println("--------------------------");
+
+        MyThreadTwo myThreadTwo1 = new MyThreadTwo();
+        System.out.println("Name of myTreadTwo1 = " + myThreadTwo1.getName() + "\n" +
+                "Priority of myThreadTwo1 = " + myThreadTwo1.getPriority());
     }
 }
---
+++ src/main/java/javaExperiments/multithreading/thread/MyThreadTwo.java
@@ -0,0 +1,8 @@
+package javaExperiments.multithreading.thread;
+
+public class MyThreadTwo extends Thread{
+    @Override
+    public void run() {
+        System.out.println("hello thread");
+    }
+}
