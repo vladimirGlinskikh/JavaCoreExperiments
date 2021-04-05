5e30529346b7394a16cd57cc84a2d7004f611cf2
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-05 22:31:01 +0600

    Methods wait() and notify() example

---
+++ src/main/java/javaExperiments/multithreading/waitAndNotify/App.java
@@ -0,0 +1,75 @@
+package javaExperiments.multithreading.waitAndNotify;
+
+public class App {
+    public static void main(String[] args) {
+        Market market = new Market();
+        Producer producer = new Producer(market);
+        Consumer consumer = new Consumer(market);
+        Thread thread1 = new Thread(producer);
+        Thread thread2 = new Thread(consumer);
+        thread1.start();
+        thread2 .start();
+    }
+}
+
+class Market {
+    private int breadCount = 0;
+
+    public synchronized void getBread() {
+        while (breadCount < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        breadCount--;
+        System.out.println("Consumer to buy bread ... ");
+        System.out.println("Amount bread on the shop = " + breadCount);
+        notify();
+    }
+
+    public synchronized void putBread() {
+        while (breadCount >= 5) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        breadCount++;
+        System.out.println("Producer added bread ...");
+        System.out.println("Amount bread on the shop = " + breadCount);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    Market market;
+
+    public Producer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.putBread();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Market market;
+
+    public Consumer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            market.getBread();
+        }
+    }
+}
