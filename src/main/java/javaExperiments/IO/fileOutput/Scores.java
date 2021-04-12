ca60a435b2192308764629d6975c4910b572baf9
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-12 07:05:34 +0600

    Output in the file

--- src/main/java/javaExperiments/IO/fileOutput/Scores.java
+++ src/main/java/javaExperiments/IO/fileOutput/Scores.java
@@ -1,5 +1,10 @@
 package javaExperiments.IO.fileOutput;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Scores {
@@ -13,19 +18,37 @@ public class Scores {
         final String INPUT_PROMPT = "\nOn each line, enter a test score (or " +
                 SENTINEL + " to quit): ";
         final String RESULT = "\n\nThe sum of the scores is: ";
+        PrintWriter printWriter = null;
         Scanner scanner = new Scanner(System.in);
-        System.out.print(INPUT_PROMPT);
-        int sum = addScores(scanner);
-        System.out.println(RESULT + sum);
+        try {
+            printWriter = new PrintWriter(new BufferedWriter(new FileWriter("/home/vg/Desktop/scores.txt")));
+            System.out.print(INPUT_PROMPT);
+        } catch (IOException e) {
+            System.err.println(e);
+        } finally {
+            int sum = addScores(scanner, printWriter);
+            printWriter.println(RESULT + sum);
+            printWriter.close();
+        }
     }
 
-    public int addScores(Scanner scanner) {
+    public int addScores(Scanner scanner, PrintWriter printWriter) {
         int score, sum = 0;
+        boolean atLeastOneScore = false;
         while (true) {
-            score = scanner.nextInt();
-            if (score == SENTINEL)
-                break;
-            sum += score;
+            try {
+                score = scanner.nextInt();
+                if (score == SENTINEL)
+                    break;
+                printWriter.println(score);
+                sum += score;
+                atLeastOneScore = true;
+            } catch (InputMismatchException e) {
+                printWriter.println(e + " " + scanner.nextLine());
+            }
+        }
+        if (!atLeastOneScore) {
+            throw new RuntimeException("The input contains no legal scores. ");
         }
         return sum;
     }
