df6e62f9bda0685a59760e568ba185e17b3cbd0a
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-04-21 21:01:48 +0600

    Check done

--- src/main/java/javaRushAnswers/App1.java
+++ src/main/java/javaRushAnswers/App1.java
@@ -3,10 +3,13 @@ package javaRushAnswers;
 public class App1 {
     public static void main(String[] args) {
         int[] array = new int[5201];
+        int count = 0;
         for (int i = 1; i < array.length; i++) {
             if (i % 13 == 0) {
                 array[i] = i;
-                System.out.print(array[i] + " ");
+                count++;
+                System.out.print(i + " ");
+                System.out.println(count);
             }
         }
     }
