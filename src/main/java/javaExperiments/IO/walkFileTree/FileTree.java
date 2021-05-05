43da8c36860cff20791d7ca4451cbfbc149554b4
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-05-05 23:11:15 +0600

    Files.walkFileTree() method example with copying

--- src/main/java/javaExperiments/IO/walkFileTree/FileTree.java
+++ src/main/java/javaExperiments/IO/walkFileTree/FileTree.java
@@ -6,8 +6,13 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileTree {
     public static void main(String[] args) throws IOException {
-        Path path = Paths.get("/home/vg/Desktop/githowto");
-        Files.walkFileTree(path, new MyFileVisitor());
+//        Path path = Paths.get("/home/vg/Desktop/githowto");
+//        Files.walkFileTree(path, new MyFileVisitor());
+
+        Path source = Paths.get("/home/vg/Desktop/githowto");
+        Path destination = Paths.get("/home/vg/Desktop/CopyHowTo");
+        Files.walkFileTree(source, new CopyFileVisitor(source, destination));
+        System.out.println("Done!");
     }
 }
 
@@ -34,6 +39,30 @@ class MyFileVisitor implements FileVisitor<Path> {
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
         System.out.println("Exit from directory: " + dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
+
+class CopyFileVisitor extends SimpleFileVisitor<Path> {
+    Path source;
+    Path destination;
+
+    public CopyFileVisitor(Path source, Path destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(dir));
+        Files.copy(dir, newDestination, StandardCopyOption.REPLACE_EXISTING);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(file));
+        Files.copy(file, newDestination, StandardCopyOption.REPLACE_EXISTING);
         return FileVisitResult.CONTINUE;
     }
 }
