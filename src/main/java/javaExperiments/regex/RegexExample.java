5eb1a3b96e2f072e2c56a9cce663d324c880b7c8
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-05-11 21:41:34 +0600

    Regex example

--- src/main/java/javaExperiments/regex/RegexExample.java
+++ src/main/java/javaExperiments/regex/RegexExample.java
@@ -6,13 +6,18 @@ import java.util.regex.Pattern;
 public class RegexExample {
     public static void main(String[] args) {
         String s = "Vladimir Glinskikh, Kazakhstan, Almaty, Lenin street, 34, Flat 12," +
-                "email: vladimir.v@gmail.com, Postcode: AAD78, Phone number: +72342389234";
+                "email: vladimir.v@gmail.com, Postcode: AAD78, Phone number: +72342389234, email: v.glins@mail.ru";
 
-        Pattern pattern = Pattern.compile("\\w+");
+//        Pattern pattern = Pattern.compile("\\w+");
+//        Pattern pattern = Pattern.compile("\\b\\d{2}\\b");
+//        Pattern pattern = Pattern.compile("\\+\\d{9}");
+//        Pattern pattern = Pattern.compile("\\w+\\.\\w+@\\w+\\.(ru|com)");
+//        Pattern pattern = Pattern.compile("\\b\\D{5,}\\b");
+        Pattern pattern = Pattern.compile("\\s\\w{4,}\\w\\s");
         Matcher matcher = pattern.matcher(s);
 
         while (matcher.find()) {
-            System.out.println("position: " + matcher.start() + " " + matcher.group());
+            System.out.println(matcher.group());
         }
     }
 }
