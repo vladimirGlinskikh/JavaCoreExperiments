699322a26cbe544a07f0614fb7d8704ba637dd3f
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-08 23:21:56 +0600

    In Java 9, interfaces can also have private static methods

---
+++ src/main/java/functionalInterfaces/I4/App.java
@@ -0,0 +1,8 @@
+package functionalInterfaces.I4;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(I4.getName("Svetlana", "female"));
+        System.out.println(I4.getName("Vladimir", "male"));
+    }
+}
