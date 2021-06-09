d7f4807c20dea294c03a7d5e19b0f78d7c948631
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-09 23:10:09 +0600

    Providing static methods in Functional Interfaces

---
+++ src/main/java/functionalInterfaces/stringProcessor/StringProcessor.java
@@ -0,0 +1,22 @@
+package functionalInterfaces.stringProcessor;
+
+@FunctionalInterface
+public interface StringProcessor {
+    static boolean isLowerCase(String s) {
+        boolean result = true;
+        for (int i = 0; i < s.length() && result; ++i) {
+            result = Character.isLowerCase(s.charAt(i));
+        }
+        return result;
+    }
+
+    static boolean isUpperCase(String s) {
+        boolean result = true;
+        for (int i = 0; i < s.length() && result; ++i) {
+            result = Character.isUpperCase(s.charAt(i));
+        }
+        return result;
+    }
+
+    String process(String s);
+}
