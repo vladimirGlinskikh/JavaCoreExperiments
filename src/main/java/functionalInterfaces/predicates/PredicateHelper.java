674d6c266a1310dd1301db26c3eed8600aee4888
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-15 06:43:09 +0600

    Chains involving the ! Operation

--- src/main/java/functionalInterfaces/predicates/PredicateHelper.java
+++ src/main/java/functionalInterfaces/predicates/PredicateHelper.java
@@ -16,7 +16,7 @@ public class PredicateHelper {
 //        result(s -> s.charAt(0) == 'H', "Hello");
 //        result(y -> y >= 4.0, 3.9);
         Predicate<Integer> p1 = x -> x > 10;
-        System.out.println(p1.or(x -> x < 31).test(9));//true
+//        System.out.println(p1.or(x -> x < 31).test(9));//true
 
         result(p1.or(x -> x < 3), 9);//false
         result(p1.or(x -> x < 3), 2);//true
@@ -25,5 +25,13 @@ public class PredicateHelper {
         result(p1.and(x -> x < 3), 9);//false
         result(p1.and(x -> x < 3), 2);//false
         result(p1.and(x -> x < 3), 5);//false
+        System.out.println("---------------------");
+        System.out.println(p1.and(x -> x < 3).negate().test(9));//true !((x > 10) && (x < 3)
+        System.out.println(p1.and(x -> x < 3).negate().test(2));//true !((x > 10) && (x < 3)
+        System.out.println(p1.and(x -> x < 3).negate().test(5));//true !((x > 10) && (x < 3)
+        System.out.println("----------------------");
+        System.out.println(p1.negate().and(x -> x < 3).test(9));//false (!(x > 10) && (x < 3))
+        System.out.println(p1.negate().and(x -> x < 3).test(2));//true (!(x > 10) && (x < 3))
+        System.out.println(p1.negate().and(x -> x < 3).test(5));//false (!(x > 10) && (x < 3))
     }
 }
