a16876988c7d01bef718c8edbcc6464d43b322ea
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-16 14:07:30 +0600

    Using Predicate.isEqual

--- src/main/java/functionalInterfaces/predicates/PredicateHelper.java
+++ src/main/java/functionalInterfaces/predicates/PredicateHelper.java
@@ -16,22 +16,27 @@ public class PredicateHelper {
 //        result(s -> s.charAt(0) == 'H', "Hello");
 //        result(y -> y >= 4.0, 3.9);
         Predicate<Integer> p1 = x -> x > 10;
+        Predicate<Integer> p2 = Predicate.isEqual(5);
+        if (p2.test(5)) {
+            System.out.println("The Predicate is true");
+            System.out.println(p1.or(Predicate.isEqual(5)).test(5));
+        }
 //        System.out.println(p1.or(x -> x < 31).test(9));//true
 
-        result(p1.or(x -> x < 3), 9);//false
-        result(p1.or(x -> x < 3), 2);//true
-        result(p1.or(x -> x < 3), 5);//false
-        System.out.println("---------------------");
-        result(p1.and(x -> x < 3), 9);//false
-        result(p1.and(x -> x < 3), 2);//false
-        result(p1.and(x -> x < 3), 5);//false
-        System.out.println("---------------------");
-        System.out.println(p1.and(x -> x < 3).negate().test(9));//true !((x > 10) && (x < 3)
-        System.out.println(p1.and(x -> x < 3).negate().test(2));//true !((x > 10) && (x < 3)
-        System.out.println(p1.and(x -> x < 3).negate().test(5));//true !((x > 10) && (x < 3)
-        System.out.println("----------------------");
-        System.out.println(p1.negate().and(x -> x < 3).test(9));//false (!(x > 10) && (x < 3))
-        System.out.println(p1.negate().and(x -> x < 3).test(2));//true (!(x > 10) && (x < 3))
-        System.out.println(p1.negate().and(x -> x < 3).test(5));//false (!(x > 10) && (x < 3))
+//        result(p1.or(x -> x < 3), 9);//false
+//        result(p1.or(x -> x < 3), 2);//true
+//        result(p1.or(x -> x < 3), 5);//false
+//        System.out.println("---------------------");
+//        result(p1.and(x -> x < 3), 9);//false
+//        result(p1.and(x -> x < 3), 2);//false
+//        result(p1.and(x -> x < 3), 5);//false
+//        System.out.println("---------------------");
+//        System.out.println(p1.and(x -> x < 3).negate().test(9));//true !((x > 10) && (x < 3)
+//        System.out.println(p1.and(x -> x < 3).negate().test(2));//true !((x > 10) && (x < 3)
+//        System.out.println(p1.and(x -> x < 3).negate().test(5));//true !((x > 10) && (x < 3)
+//        System.out.println("----------------------");
+//        System.out.println(p1.negate().and(x -> x < 3).test(9));//false (!(x > 10) && (x < 3))
+//        System.out.println(p1.negate().and(x -> x < 3).test(2));//true (!(x > 10) && (x < 3))
+//        System.out.println(p1.negate().and(x -> x < 3).test(5));//false (!(x > 10) && (x < 3))
     }
 }
