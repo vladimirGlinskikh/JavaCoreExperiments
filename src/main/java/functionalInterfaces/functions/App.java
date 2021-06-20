2bc0bcb5050927b1e662db9988e208660a74d336
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-20 22:55:59 +0600

    Specializations of Functions which convert to primitive types

--- src/main/java/functionalInterfaces/functions/App.java
+++ src/main/java/functionalInterfaces/functions/App.java
@@ -1,15 +1,11 @@
 package functionalInterfaces.functions;
 
-import java.util.function.DoubleFunction;
-import java.util.function.Function;
-import java.util.function.IntFunction;
-import java.util.function.LongFunction;
+import java.util.function.*;
 
 public class App {
     public static void main(String[] args) {
         Function<String, Integer> function = Integer::parseInt;
         Integer i = function.apply("101");
-        System.out.println(i);
 
         Function<Boolean, Integer> fbi = x -> x ? 1 : 0;
         Function<String, Boolean> fsb = Boolean::parseBoolean;
@@ -19,12 +15,22 @@ public class App {
         DoubleFunction<Boolean> fd = x -> x > 5.0;
         LongFunction<Integer> fl = x -> (int) x;
 
-//        System.out.println(fsb.andThen(x -> x ? 1 : 0).apply("true"));
+        ToIntFunction<String> ti = Integer::parseInt;
+        ToLongFunction<Double> tl = Double::longValue;
+        ToDoubleFunction<Integer> td = x -> (Integer.valueOf(x).doubleValue());
+
+        System.out.println(i);
+        System.out.println("-------------------------");
+        System.out.println(fsb.andThen(x -> x ? 1 : 0).apply("true"));
         System.out.println(fbi.compose(fsb).apply("true"));
         System.out.println(fss.apply("Hello Function interfaces"));
-
+        System.out.println("------------------------");
         System.out.println(fi.apply(5));
         System.out.println(fd.apply(5.9));
         System.out.println(fl.apply(20L));
+        System.out.println("--------------------------");
+        System.out.println(ti.applyAsInt("5"));
+        System.out.println(tl.applyAsLong(5.1));
+        System.out.println(td.applyAsDouble(7));
     }
 }
