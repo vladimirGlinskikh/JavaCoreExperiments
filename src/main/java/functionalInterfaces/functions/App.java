dbb06347d00500595fb421e5472308343a854c87
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-20 22:45:11 +0600

    Specializations of functions which convert from primitive types

--- src/main/java/functionalInterfaces/functions/App.java
+++ src/main/java/functionalInterfaces/functions/App.java
@@ -1,6 +1,9 @@
 package functionalInterfaces.functions;
 
+import java.util.function.DoubleFunction;
 import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.LongFunction;
 
 public class App {
     public static void main(String[] args) {
@@ -11,8 +14,17 @@ public class App {
         Function<Boolean, Integer> fbi = x -> x ? 1 : 0;
         Function<String, Boolean> fsb = Boolean::parseBoolean;
         Function<String, String> fss = Function.identity();
+
+        IntFunction<String> fi = x -> (Integer.valueOf(x)).toString();
+        DoubleFunction<Boolean> fd = x -> x > 5.0;
+        LongFunction<Integer> fl = x -> (int) x;
+
 //        System.out.println(fsb.andThen(x -> x ? 1 : 0).apply("true"));
         System.out.println(fbi.compose(fsb).apply("true"));
         System.out.println(fss.apply("Hello Function interfaces"));
+
+        System.out.println(fi.apply(5));
+        System.out.println(fd.apply(5.9));
+        System.out.println(fl.apply(20L));
     }
 }
