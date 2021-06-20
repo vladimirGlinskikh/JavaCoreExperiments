f8c0abaeb7b7c36ceb891f71b7739ca95621dd25
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-20 22:33:09 +0600

    The Function.identity method

--- src/main/java/functionalInterfaces/functions/App.java
+++ src/main/java/functionalInterfaces/functions/App.java
@@ -10,7 +10,9 @@ public class App {
 
         Function<Boolean, Integer> fbi = x -> x ? 1 : 0;
         Function<String, Boolean> fsb = Boolean::parseBoolean;
+        Function<String, String> fss = Function.identity();
 //        System.out.println(fsb.andThen(x -> x ? 1 : 0).apply("true"));
         System.out.println(fbi.compose(fsb).apply("true"));
+        System.out.println(fss.apply("Hello Function interfaces"));
     }
 }
