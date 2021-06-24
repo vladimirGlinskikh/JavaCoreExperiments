c65da2fbcee3eb66dea036b1749a81d387550120
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-06-24 21:38:19 +0600

    Specializations of UnaryOperator

--- src/main/java/functionalInterfaces/operators/App.java
+++ src/main/java/functionalInterfaces/operators/App.java
@@ -1,5 +1,8 @@
 package functionalInterfaces.operators;
 
+import java.util.function.DoubleUnaryOperator;
+import java.util.function.IntUnaryOperator;
+import java.util.function.LongUnaryOperator;
 import java.util.function.UnaryOperator;
 
 public class App {
@@ -8,11 +11,21 @@ public class App {
         UnaryOperator<Integer> increment = x -> ++x;
         UnaryOperator<Long> decrement = x -> --x;
 
+        IntUnaryOperator iuo = x -> x + 5;
+        LongUnaryOperator luo = x -> x / 3L;
+        DoubleUnaryOperator duo = x -> x * 3.2;
+
         System.out.println(concat.apply("Function Java "));
         System.out.println(increment.apply(4));
         System.out.println(decrement.apply(7L));
 
         UnaryOperator<String> sident = UnaryOperator.identity();
         System.out.println(sident.apply("Function Java"));
+        System.out.println("------------------------");
+
+        System.out.println(iuo.applyAsInt(4));
+        System.out.println(luo.applyAsLong(9L));
+        System.out.println(duo.applyAsDouble(4.1));
+        System.out.println("------------------------");
     }
 }
