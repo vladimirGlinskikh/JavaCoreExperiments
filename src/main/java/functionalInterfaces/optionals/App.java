553646fcef0f5acd8205e9094ff4a184dd76a861
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-07-11 06:04:18 +0600

    Creating an Optional interface

---
+++ src/main/java/functionalInterfaces/optionals/App.java
@@ -0,0 +1,22 @@
+package functionalInterfaces.optionals;
+
+import java.util.Optional;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            Optional<String> optional1 = Optional.of(null);
+        }catch (NullPointerException e){
+            System.out.println("NullPointerException");
+        }
+        Optional<String> optional2 = Optional.of("Hello Optional interface");
+        System.out.println(optional2);
+
+        Optional<String> optional3 = Optional.ofNullable(null);
+        System.out.println(optional3);
+        Optional<String> optional4 = Optional.ofNullable("Hello");
+        System.out.println(optional4);
+        Optional<String> optional5 = Optional.empty();
+        System.out.println(optional5);
+    }
+}
