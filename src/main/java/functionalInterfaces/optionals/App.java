b29fd24a78c0231c88a6d9cddbda604741a89413
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-07-12 23:29:26 +0600

    Creating Chains of Optionals

--- src/main/java/functionalInterfaces/optionals/App.java
+++ src/main/java/functionalInterfaces/optionals/App.java
@@ -2,6 +2,8 @@ package functionalInterfaces.optionals;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Supplier;
 
 public class App {
     public static void main(String[] args) {
@@ -33,14 +35,40 @@ public class App {
 //            System.out.println("NoSuchElementException");
 //        }
 
-        if (optional.isPresent())
-            System.out.println(optional.get());
-        if (optional1.isPresent())
-            System.out.println(optional1.get());
-        else System.out.println("optional1 is null");
+//        if (optional.isPresent())
+//            System.out.println(optional.get());
+//        if (optional1.isPresent())
+//            System.out.println(optional1.get());
+//        else System.out.println("optional1 is null");
+//
+//        if (!optional.isEmpty())
+//            System.out.println(optional.get());
+//        else System.out.println("optional is null");
 
-        if (!optional.isEmpty())
-            System.out.println(optional.get());
-        else System.out.println("optional is null");
+//        String t = null, u = "Java";
+//        String s = t;
+//        if (t == null)
+//            s = u;
+//        System.out.println(s);
+//        String s = Optional.ofNullable(t).orElse(u);
+//        System.out.println(s);
+
+//        try {
+//            String s = null;
+//            String opt = Optional.ofNullable(s)
+//                    .orElseThrow(/*() -> new Exception("Null Optional")*/);
+//        } catch (Exception e) {
+//            System.err.println(e.getMessage());
+//        }
+
+        Supplier<Optional<String>> supplier = () -> {
+            System.out.println("Enter a string:");
+            return Optional.of((new Scanner(System.in)).nextLine());
+        };
+
+        String s = null;
+        Optional<String> os = Optional.ofNullable(s).or(supplier);
+        if (os.isPresent())
+            System.out.println(os.get());
     }
 }
