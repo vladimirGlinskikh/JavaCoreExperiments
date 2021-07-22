e07ac8943b2b626a77c89287b8596ccc139bb57d
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-07-22 22:45:42 +0600

    Color parametrization

--- src/main/java/modernJava/filteringGreenApples/App.java
+++ src/main/java/modernJava/filteringGreenApples/App.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static modernJava.filteringGreenApples.Color.GREEN;
+import static modernJava.filteringGreenApples.Color.RED;
 
 public class App {
     public static List<Apple> filterGreenApples(List<Apple> inventory) {
@@ -17,13 +18,28 @@ public class App {
         return result;
     }
 
+    public static List<Apple> filterApplesByColor(List<Apple> inventory, Color color) {
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if (apple.getColor().equals(color)) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         List<Apple> inventory = Arrays.asList(
                 new Apple(80, Color.GREEN),
                 new Apple(155, Color.GREEN),
                 new Apple(120, Color.RED));
 
-        List<Apple> greenApples = filterGreenApples(inventory);
+//        List<Apple> greenApples = filterGreenApples(inventory);
+//        System.out.println(greenApples);
+
+        List<Apple> greenApples = filterApplesByColor(inventory, GREEN);
+        List<Apple> redApples = filterApplesByColor(inventory, RED);
         System.out.println(greenApples);
+        System.out.println(redApples);
     }
 }
