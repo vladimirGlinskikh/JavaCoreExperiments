a7231d3d6eb0d3961dd7e99d921e4f536da53e93
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-04 22:49:37 +0600

    Linking several functions into one

--- src/main/java/functionalJava/functionalInterface/FunctionExample.java
+++ src/main/java/functionalJava/functionalInterface/FunctionExample.java
@@ -5,13 +5,24 @@ import java.util.function.Function;
 public class FunctionExample {
     static Function<Integer, Integer> incrementByOneFunction = number -> ++number;
 
+    static int increment(int number) {
+        return number + 1;
+    }
+    static Function<Integer, Integer> multiplyByTenFunction = number -> number * 10;
+
     public static void main(String[] args) {
         int increment = increment(0);
         System.out.println(increment);
-        System.out.println(incrementByOneFunction.apply(2));
-    }
 
-    static int increment(int number) {
-        return number + 1;
+        int increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        int multiply = multiplyByTenFunction.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addByOneAndThenMultiplyByTen = incrementByOneFunction.andThen(multiplyByTenFunction);
+        System.out.println(addByOneAndThenMultiplyByTen.apply(3));
+//        System.out.println(addByOneAndThenMultiplyByTen);
+
     }
 }
