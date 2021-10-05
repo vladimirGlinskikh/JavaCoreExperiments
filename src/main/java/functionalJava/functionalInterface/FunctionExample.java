dbfcea1053124e9322b4063dee11b47f18c7fc55
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-05 19:30:01 +0600

    BiFunction example

--- src/main/java/functionalJava/functionalInterface/FunctionExample.java
+++ src/main/java/functionalJava/functionalInterface/FunctionExample.java
@@ -1,5 +1,6 @@
 package functionalJava.functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class FunctionExample {
@@ -8,6 +9,17 @@ public class FunctionExample {
     static int increment(int number) {
         return number + 1;
     }
+
+    // BiFunction declarative approach
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+
+    // BiFunction imperative approach
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number + 1) * numToMultiplyBy;
+    }
+
     static Function<Integer, Integer> multiplyByTenFunction = number -> number * 10;
 
     public static void main(String[] args) {
@@ -23,6 +35,13 @@ public class FunctionExample {
         Function<Integer, Integer> addByOneAndThenMultiplyByTen = incrementByOneFunction.andThen(multiplyByTenFunction);
         System.out.println(addByOneAndThenMultiplyByTen.apply(3));
 //        System.out.println(addByOneAndThenMultiplyByTen);
+
+        System.out.println("---------------------------------------");
+        // BiFunction imperative approach
+        System.out.println(incrementByOneAndMultiply(4, 100));
+
+        // BiFunction declarative approach
+        System.out.println(incrementByOneAndMultiplyBiFunction.apply(4, 100));
 
     }
 }
