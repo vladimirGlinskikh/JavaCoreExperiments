a50a0890a302fa6f88a4753a85914d920de47005
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-10-10 13:52:01 +0600

    Using combinator patter with interface

--- src/main/java/functionalJava/combinatorPattern/Main.java
+++ src/main/java/functionalJava/combinatorPattern/Main.java
@@ -2,6 +2,8 @@ package functionalJava.combinatorPattern;
 
 import java.time.LocalDate;
 
+import static functionalJava.combinatorPattern.CustomerRegistrationValidator.*;
+
 public class Main {
     public static void main(String[] args) {
         Customer customer = new Customer(
@@ -10,6 +12,16 @@ public class Main {
                 "+77014568967",
                 LocalDate.of(1990, 1, 1));
 
-        System.out.println(new CustomerValidatorService().isValid(customer));
+//        System.out.println(new CustomerValidatorService().isValid(customer));
+
+        ValidationResult result = isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .apply(customer);
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
     }
 }
