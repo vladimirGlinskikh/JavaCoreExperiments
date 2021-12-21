3732c2d33bbe96e2acb5bc8945767ca3bd10af00
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-12-21 21:04:28 +0600

    Set in action

--- src/main/java/core/collection/App.java
+++ src/main/java/core/collection/App.java
@@ -1,18 +1,66 @@
 package core.collection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
 public class App {
     public static void main(String[] args) {
-        Map<String, Integer> map = new HashMap<>();
+//        System.out.println(nCopies(4, 5));
+        System.out.println(rangeSet(10, 20).contains(10));
+        System.out.println(rangeSet(10, 20).contains(20));
+        System.out.println(rangeSet(10, 20));
+        System.out.println(rangeSet(0, Integer.MAX_VALUE).contains(-1));
+    }
 
-        System.out.println( map.put("килька", 17));
-        System.out.println( map.put("сардина", 14));
-        System.out.println( map.put("сардина", 7));
+    static <T> Iterable<T> nCopies(T value, int count) {
+        if (count < 0)
+            throw new IllegalArgumentException("Negative count: " + count);
+        return () -> new Iterator<>() {
+            int rest = count;
 
-        System.out.println(map);
-        System.out.println(map.size());
-        System.out.println(map.isEmpty());
+            @Override
+            public boolean hasNext() {
+                return rest > 0;
+            }
+
+            @Override
+            public T next() {
+                if (rest == 0)
+                    throw new NoSuchElementException();
+                rest--;
+                return value;
+            }
+        };
+    }
+
+    static Set<Integer> rangeSet(int fromInclusive, int toExclusive) {
+        return new AbstractSet<>() {
+            @Override
+            public Iterator<Integer> iterator() {
+                return new Iterator<>() {
+                    int next = fromInclusive;
+
+                    @Override
+                    public boolean hasNext() {
+                        return next != toExclusive;
+                    }
+
+                    @Override
+                    public Integer next() {
+                        if (next == toExclusive) {
+                            throw new NoSuchElementException();
+                        }
+                        return next++;
+                    }
+                };
+            }
+
+            @Override
+            public int size() {
+                return toExclusive - fromInclusive;
+            }
+        };
     }
 }
