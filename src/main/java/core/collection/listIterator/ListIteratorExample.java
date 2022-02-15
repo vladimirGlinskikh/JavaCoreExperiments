0113e72e24993c76a018d612d3f100f4b12400ce
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-02-15 23:21:31 +0600

    ListIterator in action

---
+++ src/main/java/core/collection/listIterator/ListIteratorExample.java
@@ -0,0 +1,29 @@
+package core.collection.listIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class ListIteratorExample {
+    public static void main(String[] args) {
+        String s = "World";
+        List<Character> list = new ArrayList<>();
+        for (char ch : s.toCharArray()) {
+            list.add(ch);
+        }
+        ListIterator<Character> iterator = list.listIterator();
+        ListIterator<Character> reverseIterator = list.listIterator(list.size());
+        boolean isPalindrome = true;
+        while (iterator.hasNext() && reverseIterator.hasPrevious()) {
+            if (iterator.next() != reverseIterator.previous()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("It is a palindrome");
+        } else {
+            System.out.println("It is not a palindrome");
+        }
+    }
+}
