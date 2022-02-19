6d9f496fd81501386823fe07aa16acf542dfd827
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-02-20 02:44:32 +0600

    Check done

---
+++ src/main/java/sololearn/incrementAndDecrement/Main.java
@@ -0,0 +1,18 @@
+package sololearn.incrementAndDecrement;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int initScore = scanner.nextInt();
+        int scoreTom = initScore;
+        int scoreBob = initScore;
+
+        System.out.println("Round 1 results:");
+        //fix
+        System.out.println(++scoreTom);
+        System.out.println(--scoreBob);
+    }
+}
