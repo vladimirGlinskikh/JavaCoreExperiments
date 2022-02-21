902c4838d4cd042a32216454e2d172d5bde877ec
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-02-21 23:07:20 +0600

    Refactor code

--- src/main/java/javaRushAnswers/task0824/Solution.java
+++ src/main/java/javaRushAnswers/task0824/Solution.java
@@ -2,28 +2,42 @@ package javaRushAnswers.task0824;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        Human child = new Human("Игорь", true, 25, null);
-        Human child1 = new Human("Артем", true, 20, null);
-        Human child2 = new Human("Петя", true, 10, null);
-        Human father = new Human("Валерий", true, 45, new ArrayList<>(Arrays.asList(child, child1, child2)));
-        Human mother = new Human("Елена", false, 40, new ArrayList<>(Arrays.asList(child, child1, child2)));
-        Human grandFather1 = new Human("Василий", true, 70, new ArrayList<>(Arrays.asList(mother)));
-        Human grandMother1 = new Human("Мария", false, 65, new ArrayList<>(Arrays.asList(mother)));
-        Human grandFather2 = new Human("Николай", true, 85, new ArrayList<>(Arrays.asList(father)));
-        Human grandMother2 = new Human("Люся", false, 80, new ArrayList<>(Arrays.asList(father)));
-        System.out.println(child);
+        Human child1 = new Human("Игорь", true, 25, null);
+        Human child2 = new Human("Артем", true, 20, null);
+        Human child3 = new Human("Петя", true, 10, null);
+
+        ArrayList<Human> child = new ArrayList<>();
+        child.add(child1);
+        child.add(child2);
+        child.add(child3);
+
+        Human father = new Human("Валерий", true, 45, child);
+        Human mother = new Human("Елена", false, 40, child);
+
+        ArrayList<Human> parent1 = new ArrayList<>();
+        parent1.add(father);
+        ArrayList<Human> parent2 = new ArrayList<>();
+        parent2.add(mother);
+
+
+        Human grandFather1 = new Human("Василий", true, 70, parent1);
+        Human grandMother1 = new Human("Мария", false, 65, parent1);
+
+        Human grandMother2 = new Human("Люся", false, 80, parent2);
+        Human grandFather2 = new Human("Николай", true, 85, parent2);
+
+        System.out.println(grandFather1);
+        System.out.println(grandFather2);
+        System.out.println(grandMother1);
+        System.out.println(grandMother2);
+        System.out.println(father);
+        System.out.println(mother);
         System.out.println(child1);
         System.out.println(child2);
-        System.out.println(mother);
-        System.out.println(father);
-        System.out.println(grandFather1);
-        System.out.println(grandMother1);
-        System.out.println(grandFather2);
-        System.out.println(grandMother2);
+        System.out.println(child3);
     }
 
     public static class Human {
