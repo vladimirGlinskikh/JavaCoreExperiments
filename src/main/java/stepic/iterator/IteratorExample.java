433b608385e8b457cd832ddae39fdf23aac2b6c0
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-09 23:31:15 +0600

    Task is done

---
+++ src/main/java/stepic/iterator/IteratorExample.java
@@ -0,0 +1,29 @@
+package stepic.iterator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class IteratorExample {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String number = reader.readLine();
+        String[] parts = number.split(" ");
+
+        List<Integer> list = new ArrayList<>();
+        for (String str : parts) {
+            list.add(Integer.valueOf(str));
+        }
+
+        Collections.sort(list);
+
+        for (Integer temp : list) {
+            int elem = Integer.parseInt(String.valueOf(temp));
+            if (elem % 2 != 0) {
+                System.out.println(temp);
+            }
+        }
+        return;
+    }
+}
---
+++ src/main/java/stepic/iterator/task.md
@@ -0,0 +1,20 @@
+ Using BufferedReader read one string with numbers written across the space `("1 2 3 4 5 ...")`.
+
+ Put this numbers to any `Collection` of `Integers.Sort` this collection!
+
+ Using Iterator remove all even annoying numbers`(death for elem%2=0 !)` from your `Collection`
+
+ Print all remaining elements to console `(use System.out.println() method)`
+___________________________________________________________
+
+ Sample Input:
+
+ 1 2 3 4 5 6 7 8 9 10
+ _____________________________________________________________
+ Sample Output:
+
+1<br/>
+3<br/>
+5<br/>
+7<br/>
+9
