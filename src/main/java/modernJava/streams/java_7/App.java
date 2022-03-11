a2eb69a62e6e3860f47d183db0b8062b2c1fb1d1
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-11 13:04:29 +0600

    Refactor

--- src/main/java/modernJava/streams/java_7/App.java
+++ src/main/java/modernJava/streams/java_7/App.java
@@ -1,30 +1,24 @@
 package modernJava.streams.java_7;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        getLowCaloricDishesNameInJava7(Dishes.menu)
+        getLowCaloricDishesNameInJava7()
                 .forEach(System.out::println);
     }
 
-    private static List<String> getLowCaloricDishesNameInJava7(List<Dishes> dishes) {
+    private static List<String> getLowCaloricDishesNameInJava7() {
         List<Dishes> lowCaloricDishes = new ArrayList<>();
-        for (Dishes dish : dishes) {
+        for (Dishes dish : Dishes.menu) {
             if (dish.getCalories() < 400) {
                 lowCaloricDishes.add(dish);
             }
         }
 
-        Collections.sort(lowCaloricDishes, new Comparator<Dishes>() {
-            @Override
-            public int compare(Dishes dish1, Dishes dish2) {
-                return Integer.compare(dish1.getCalories(), dish2.getCalories());
-            }
-        });
+        lowCaloricDishes.sort(Comparator.comparingInt(Dishes::getCalories));
 
         List<String> lowCaloricDishesName = new ArrayList<>();
         for (Dishes dish : lowCaloricDishes) {
