8a3498fbb5f4f023641fc9104c96bed6278da93b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-11 13:18:25 +0600

    Streams after Java 8

--- src/main/java/modernJava/streams/java_7/App.java
+++ src/main/java/modernJava/streams/java_7/App.java
@@ -3,11 +3,27 @@ package modernJava.streams.java_7;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
 
 public class App {
     public static void main(String[] args) {
         getLowCaloricDishesNameInJava7()
                 .forEach(System.out::println);
+
+        System.out.println("-----------------------------");
+
+        getLowCaloricDishesNameInJava8()
+                .forEach(System.out::println);
+    }
+
+    private static List<String> getLowCaloricDishesNameInJava8() {
+        return Dishes.menu.stream()
+                .filter(dishes -> dishes.getCalories() < 400)
+                .sorted(comparing(Dishes::getCalories))
+                .map(Dishes::getName)
+                .collect(Collectors.toList());
     }
 
     private static List<String> getLowCaloricDishesNameInJava7() {
