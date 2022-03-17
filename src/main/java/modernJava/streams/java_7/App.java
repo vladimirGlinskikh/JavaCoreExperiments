43eed04997f763da30d2a18d29cb3c3f0362ce70
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-17 22:00:29 +0600

    Searching for an item in a stream

--- src/main/java/modernJava/streams/java_7/App.java
+++ src/main/java/modernJava/streams/java_7/App.java
@@ -1,12 +1,10 @@
 package modernJava.streams.java_7;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
+import static modernJava.streams.java_7.Dishes.menu;
 
 public class App {
     public static void main(String[] args) {
@@ -47,14 +45,20 @@ public class App {
 
         System.out.println("-----------------------------");
 
+        findAnyVegetarianDish();
+    }
 
+    private static void findAnyVegetarianDish() {
+        menu.stream()
+                .filter(Dishes::isVegetarian)
+                .findAny()
+                .ifPresent(dishes -> System.out.println(dishes.getName()));
     }
 
     private static void whetherMenusGoodForYourHealth() {
-        if (Dishes.menu.stream()
+        if (menu.stream()
 //                .allMatch(dishes -> dishes.getCalories() < 1_000))
-                .noneMatch(dishes -> dishes.getCalories() <= 700))
-        {
+                .noneMatch(dishes -> dishes.getCalories() <= 700)) {
             System.out.println("The menu is good for your health");
         } else {
             System.out.println("The menu is not good for your health");
@@ -62,27 +66,27 @@ public class App {
     }
 
     private static void isVegetarianChecking() {
-        if (Dishes.menu.stream()
+        if (menu.stream()
                 .anyMatch(Dishes::isVegetarian)) {
             System.out.println("The menu is (somewhat) vegetarian friendly!");
         }
     }
 
     private static List<Dishes> getSkipThreeHighCaloricDishNames() {
-        return Dishes.menu.stream()
+        return menu.stream()
                 .filter(dishes1 -> dishes1.getCalories() > 300)
                 .skip(3)
                 .collect(Collectors.toList());
     }
 
     private static List<Dishes> getSliceMenu() {
-        return Dishes.menu.stream()
+        return menu.stream()
                 .dropWhile(dishes -> dishes.getCalories() < 400)
                 .collect(Collectors.toList());
     }
 
     private static List<String> getThreeHighCaloricDishNames() {
-        return Dishes.menu.stream()
+        return menu.stream()
                 .filter(dishes -> dishes.getCalories() > 300)
                 .map(Dishes::getName)
                 .limit(3)
@@ -90,12 +94,12 @@ public class App {
     }
 
     private static Map<Dishes.Type, List<Dishes>> getDishesByType() {
-        return Dishes.menu.stream()
+        return menu.stream()
                 .collect(Collectors.groupingBy(Dishes::getType));
     }
 
     private static List<String> getLowCaloricDishesNameInJava8() {
-        return Dishes.menu.stream()
+        return menu.stream()
                 .filter(dishes -> dishes.getCalories() < 400)
                 .sorted(comparing(Dishes::getCalories))
                 .map(Dishes::getName)
@@ -104,7 +108,7 @@ public class App {
 
     private static List<String> getLowCaloricDishesNameInJava7() {
         List<Dishes> lowCaloricDishes = new ArrayList<>();
-        for (Dishes dish : Dishes.menu) {
+        for (Dishes dish : menu) {
             if (dish.getCalories() < 400) {
                 lowCaloricDishes.add(dish);
             }
