42a28356dcb5d643a043cec995e68eb423baaf93
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-21 20:09:22 +0600

    Find all the transactions for 2011 and sort them by amount (from small to large)

---
+++ src/main/java/modernJava/streams/practics/Main.java
@@ -0,0 +1,32 @@
+package modernJava.streams.practics;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+        List<Transactional> transactions = Arrays.asList(
+                new Transactional(brian, 2011, 300),
+                new Transactional(raoul, 2012, 1_000),
+                new Transactional(raoul, 2011, 400),
+                new Transactional(mario, 2012, 710),
+                new Transactional(mario, 2012, 700),
+                new Transactional(alan, 2012, 950));
+
+        findAllTransactionsFor2011YearAndSortedThemByAmountFromSmallToLarge(transactions)
+                .forEach(System.out::println);
+    }
+
+    private static List<Transactional> findAllTransactionsFor2011YearAndSortedThemByAmountFromSmallToLarge(List<Transactional> transaction) {
+        return transaction.stream()
+                .filter(transactional -> transactional.getYear() == 2011)
+                .sorted(Comparator.comparing(Transactional::getValue))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/modernJava/streams/practics/Transactional.java
@@ -0,0 +1,32 @@
+package modernJava.streams.practics;
+
+public class Transactional {
+    private final Trader trader;
+    private final int year;
+    private final int value;
+
+    public Transactional(Trader trader, int year, int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + this.trader + ", " +
+                "year: " + this.year + ", " +
+                "value: " + this.value + "}";
+    }
+}
