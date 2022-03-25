ec05900cc7d26f2a4ae1324be7bc8ee029c74a4b
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-03-25 22:55:50 +0600

    Pythagorean triplet example

--- src/main/java/modernJava/streams/pythagoreanTriplet/PythagoreanTripletExample.java
+++ src/main/java/modernJava/streams/pythagoreanTriplet/PythagoreanTripletExample.java
@@ -5,7 +5,8 @@ import java.util.stream.Stream;
 
 public class PythagoreanTripletExample {
     public static void main(String[] args) {
-        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100).boxed()
+        Stream<int[]> pythagoreanTriples = IntStream.rangeClosed(1, 100)
+                .boxed()
                 .flatMap(a -> IntStream.rangeClosed(a, 100)
                         .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
                         .mapToObj(b ->
