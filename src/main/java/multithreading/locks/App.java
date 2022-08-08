e530ce1fe4c7435e34620d659c9a6e6c02bdaeee
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-08-08 12:06:04 +0300

    Multiple Locks, Using Synchronized Code Blocks

---
+++ src/main/java/multithreading/locks/App.java
@@ -0,0 +1,7 @@
+package multithreading.locks;
+
+public class App {
+    public static void main(String[] args) {
+        new Worker().main();
+    }
+}
---
+++ src/main/java/multithreading/locks/Worker.java
@@ -0,0 +1,63 @@
+package multithreading.locks;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Worker {
+    private Object lock1 = new Object();
+    private Object lock2 = new Object();
+    private Random random = new Random();
+    private List<Integer> list1 = new ArrayList<>();
+    private List<Integer> list2 = new ArrayList<>();
+
+    public void stageOne() {
+        synchronized (lock1) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list1.add(random.nextInt(100));
+        }
+    }
+
+    public void stageTwo() {
+        synchronized (lock2) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list2.add(random.nextInt(100));
+        }
+    }
+
+    public void process() {
+        for (int i = 0; i < 1_000; i++) {
+            stageOne();
+            stageTwo();
+        }
+    }
+
+    public void main() {
+        System.out.println("Starting: ");
+        long start = System.currentTimeMillis();
+
+        Thread t1 = new Thread(() -> process());
+
+        Thread t2 = new Thread(() -> process());
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time take: " + (end - start));
+        System.out.println("List1: " + list1.size() + "\nList2: " + list2.size());
+    }
+}
