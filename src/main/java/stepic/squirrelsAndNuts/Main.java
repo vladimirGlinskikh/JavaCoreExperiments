d7eb082ee077f13265bfb4510e14b566dfb9c2aa
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2022-09-14 13:54:44 +0300

    Check done!

---
+++ src/main/java/stepic/squirrelsAndNuts/Main.java
@@ -0,0 +1,12 @@
+package stepic.squirrelsAndNuts;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        System.out.println(k%n);
+    }
+}
