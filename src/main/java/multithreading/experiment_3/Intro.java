7d507f70ab8638500751b94715eb952695ed6415
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-01-25 21:42:09 +0300

    Wait and notify in action

--- src/main/java/multithreading/experiment_3/Intro.java
+++ src/main/java/multithreading/experiment_3/Intro.java
@@ -1,17 +1,21 @@
 package multithreading.experiment_3;
 
 public class Intro {
-    private final Object monitor = new Object();
-    private volatile int count = 0;
+    private final Lock lock = new Lock();
+    private int count = 0;
 
     public static void main(String[] args) {
         new Intro().doCounter();
     }
 
     private void increment() {
-        synchronized (monitor) {
-            count++;
+        try {
+            lock.lock();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        count++;
+        lock.unlock();
     }
 
     private void doCounter() {
@@ -31,5 +35,25 @@ public class Intro {
 
         thread1.start();
         thread2.start();
+    }
+}
+
+class Lock {
+    private boolean isLocked = false;
+
+    void lock() throws InterruptedException {
+        synchronized (this) {
+            while (isLocked) {
+                wait();
+            }
+            isLocked = true;
+        }
+    }
+
+    void unlock() {
+        synchronized (this) {
+            isLocked = false;
+            notify();
+        }
     }
 }
